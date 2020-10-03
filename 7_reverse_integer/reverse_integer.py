5c2122a74ac1cb80a9c09a052e85ae60847e2574
Alan Song <alansong95@gmail.com>
2020-10-02 22:57:05 -0700

    enhance 7_reverse_integer

--- 7_reverse_integer/reverse_integer.py
+++ 7_reverse_integer/reverse_integer.py
@@ -3,20 +3,15 @@
 # space: O(1)
 class Solution(object):
     def reverse(self, x):
-        negative = False
-        if x < 0:
-            negative = True
-            x *= -1
+        negative = x < 0
+        x = abs(x)
         y = 0
         while (x > 0):
             y = y * 10 + (x % 10)
             x = x // 10
         if self.overflow(y):
             return 0
-        elif negative:
-            return -y
-        else:
-            return y
+        return -y if negative else y 
     
     def overflow(self, x):
         if x < -(2 ** 31) or x > (2 ** 31 - 1):
