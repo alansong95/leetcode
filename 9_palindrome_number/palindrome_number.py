39b7e00967b3e9af28ef80465d207dd80bc696fa
Alan Song <alansong95@gmail.com>
2020-10-03 16:01:20 -0700

    implement 9 palindrome number without converting to str

--- 9_palindrome_number/palindrome_number.py
+++ 9_palindrome_number/palindrome_number.py
@@ -1,12 +1,27 @@
 # converting to str method
+# class Solution(object):
+#     def isPalindrome(self, x):
+#         x = str(x)
+#         rev = x[::-1]
+#         return x == rev
+        
+# without converting to str
 class Solution(object):
     def isPalindrome(self, x):
-        x = str(x)
-        rev = x[::-1]
-        return x == rev
+        if x < 0:
+            return False
+        x_copy = x
+        rev = 0
+        while x > 0:
+            rev = rev * 10 + x % 10
+            x = x // 10
+        return rev == x_copy
+
+
         
 solution = Solution()
-print(solution.isPalindrome(121))
+print(solution.isPalindrome(1111))
 print(solution.isPalindrome(-121))
 print(solution.isPalindrome(10))
+print(solution.isPalindrome(-101))
 print(solution.isPalindrome(-101))
