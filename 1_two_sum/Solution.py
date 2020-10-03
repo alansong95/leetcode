ef22aa31e8325d56a12fe65964ab0c9ef7cd8989
Alan Song <alansong95@gmail.com>
2020-10-02 22:33:53 -0700

    complete 1. two sum

---
+++ 1_two_sum/Solution.py
@@ -0,0 +1,22 @@
+# brute force
+# time: O(n^2)
+# space: O(1)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# hash map solution
+# time O(n)
+# space: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for i in range(len(nums)):
+            if target - nums[i] in hash_map:
+                return [hash_map[target - nums[i]], i]
+            else:
+                hash_map[nums[i]] = i
+                
