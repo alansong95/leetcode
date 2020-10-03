c7d9af90d6c7783b75ef1ca1e9106e5efa8d974a
Alan Song <alansong95@gmail.com>
2020-10-03 16:46:48 -0700

    13 roman to integer: implement a solution that doesd not need str reverse

--- 13_roman_to_integer/roman_to_integer.py
+++ 13_roman_to_integer/roman_to_integer.py
@@ -15,18 +15,31 @@
 #             prev = c
 #         return sum
 
+# more general solution
+# class Solution(object):
+#     _map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+#     def romanToInt(self, s):
+#         _sum = 0
+#         prev = 0
+#         for c in s[::-1]:
+#             if self._map[c] >= prev:
+#                 _sum += self._map[c]
+#             else:
+#                 _sum -= self._map[c]
+#             prev = self._map[c]
+#         return _sum
+
+# a solution that does not need reverse
 class Solution(object):
-    _map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     def romanToInt(self, s):
+        _map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         _sum = 0
-        prev = 0
-        for c in s[::-1]:
-            if self._map[c] >= prev:
-                _sum += self._map[c]
+        for i in range(len(s) - 1):
+            if _map[s[i]] < _map[s[i+1]]:
+                _sum -= _map[s[i]] 
             else:
-                _sum -= self._map[c]
-            prev = self._map[c]
-        return _sum
+                _sum += _map[s[i]]
+        return _sum + _map[s[-1]]
 
 
 solution = Solution()
