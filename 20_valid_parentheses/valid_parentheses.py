7dc54b8dbdad02ae9fee21a8226877a24519284b
Alan Song <alansong95@gmail.com>
2020-10-04 21:27:09 -0700

    20 valid parenthese: implement better solution

--- 20_valid_parentheses/valid_parentheses.py
+++ 20_valid_parentheses/valid_parentheses.py
@@ -1,14 +1,34 @@
 # brute force
 # Time: O(s): length of string
 # Space: O(1)
+# class Solution(object):
+#     def isValid(self, s):
+#         if len(s) <= 1:
+#             return False
+#         _dict = {'(': -1, '{': -1, '[': -1, ')': 1, '}': 1, ']': 1}
+#         _stack = []
+#         for c in s:
+#             if _dict[c] == -1:
+#                 _stack.append(c)
+#             else:
+#                 if len(_stack) > 0:
+#                     opener = _stack.pop()
+#                     if ord(opener)+1 == ord(c) or ord(opener)+2 == ord(c):
+#                         continue
+#                     else:
+#                         return False
+#                 else:
+#                     return False
+
+#         if len(_stack) > 0:
+#             return False
+#         return True
+
 class Solution(object):
     def isValid(self, s):
-        if len(s) <= 1:
-            return False
-        _dict = {'(': -1, '{': -1, '[': -1, ')': 1, '}': 1, ']': 1}
         _stack = []
         for c in s:
-            if _dict[c] == -1:
+            if c == '(' or c == '{' or c == '[':
                 _stack.append(c)
             else:
                 if len(_stack) > 0:
@@ -23,6 +43,7 @@ class Solution(object):
         if len(_stack) > 0:
             return False
         return True
+
 
 
 solution = Solution()
