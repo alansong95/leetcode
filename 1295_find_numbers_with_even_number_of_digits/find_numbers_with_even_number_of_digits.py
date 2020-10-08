52b470771afd714c3962c4c803fa947449f97407
Alan Song <alansong95@gmail.com>
2020-10-07 17:59:03 -0700

    implement one line solution

--- 1295_find_numbers_with_even_number_of_digits/find_numbers_with_even_number_of_digits.py
+++ 1295_find_numbers_with_even_number_of_digits/find_numbers_with_even_number_of_digits.py
@@ -1,23 +1,27 @@
 # Time: O(n)
 # Space: O(1)
+# class Solution(object):
+#     def findNumbers(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         count = 0
+#         for num in nums:
+#             if self.isEvenNumOfDigits(num):
+#                 count += 1
+#         return count
+
+#     def isEvenNumOfDigits(self, num):
+#         count = 0
+#         while num > 0:
+#             num = num // 10
+#             count += 1
+#         return count % 2 == 0
+
 class Solution(object):
     def findNumbers(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count = 0
-        for num in nums:
-            if self.isEvenNumOfDigits(num):
-                count += 1
-        return count
-
-    def isEvenNumOfDigits(self, num):
-        count = 0
-        while num > 0:
-            num = num // 10
-            count += 1
-        return count % 2 == 0
+        return len([x for x in nums if len(str(x)) % 2 == 0])
 
 solution = Solution()
 print(solution.findNumbers([12,345,2,6,7896]))
