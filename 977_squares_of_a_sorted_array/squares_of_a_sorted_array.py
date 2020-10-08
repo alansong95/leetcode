c32a010e691cd6c04bc73a1820837f93543a74fe
Alan Song <alansong95@gmail.com>
2020-10-07 18:07:26 -0700

    complete 977_squares_of_a_sorted_array/

---
+++ 977_squares_of_a_sorted_array/squares_of_a_sorted_array.py
@@ -0,0 +1,16 @@
+# Brute force
+# Time: O(nlogn)
+# Space: 1
+class Solution(object):
+    def sortedSquares(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(A)):
+            A[i] = A[i] * A[i]
+        return sorted(A)
+
+solution = Solution()
+print(solution.sortedSquares([-4,-1,0,3,10]))
+print(solution.sortedSquares([-7,-3,2,3,11]))
