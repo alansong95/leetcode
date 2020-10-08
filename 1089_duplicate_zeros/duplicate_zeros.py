99c927245112a0c5e382c2018875d29e73a32acd
Alan Song <alansong95@gmail.com>
2020-10-07 21:04:09 -0700

    1089_duplicate_zeros: implement better solution

--- 1089_duplicate_zeros/duplicate_zeros.py
+++ 1089_duplicate_zeros/duplicate_zeros.py
@@ -18,31 +18,60 @@
 
 # if constraint "in place" is there
 # brute force
+# class Solution(object):
+#     def duplicateZeros(self, arr):
+#         """
+#         :type arr: List[int]
+#         :rtype: None Do not return anything, modify arr in-place instead.
+#         """
+#         i = 0
+#         while i < len(arr): 
+#             if arr[i] == 0:
+#                 temp_prev = 0
+#                 for j in range(i+1, len(arr)):
+#                     if j < len(arr):
+#                         temp = arr[j]
+#                         arr[j] = temp_prev
+#                         temp_prev = temp
+#                 i+=1
+#             i+=1
+
+
+# if constraint "in place" is there
+# more elegant & better solution
+# Time: O(n)
+# Space: O(1)
 class Solution(object):
     def duplicateZeros(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: None Do not return anything, modify arr in-place instead.
-        """
+        arr_len = len(arr)-1
+        zero_count = 0
         i = 0
-        while i < len(arr): 
+        while i + zero_count <= arr_len:
             if arr[i] == 0:
-                temp_prev = 0
-                for j in range(i+1, len(arr)):
-                    if j < len(arr):
-                        temp = arr[j]
-                        arr[j] = temp_prev
-                        temp_prev = temp
-                i+=1
-            i+=1
+                if i == arr_len - zero_count:
+                    arr[arr_len] = 0
+                    arr_len -= 1
+                    break
+                zero_count += 1
+            i += 1
+
+        j = arr_len
+        for i in range(arr_len-zero_count, -1, -1):
+            arr[j] = arr[i]
+            if arr[i] == 0:
+                arr[j-1] = arr[i]
+                j -= 1
+            j -= 1
         print(arr)
-
-            
-            
-
-
 
 
 solution = Solution()
 solution.duplicateZeros([1,0,2,3,0,4,5,0])
 solution.duplicateZeros([1,2,3])
+solution.duplicateZeros([8,4,5,0,0,0,0,7])
+solution.duplicateZeros([8,4,5,0,0,5,0,7])
+solution.duplicateZeros([8,4,5,2,3,0,0,7])
+solution.duplicateZeros([])
+solution.duplicateZeros([0])
+solution.duplicateZeros([1])
+solution.duplicateZeros([1,0])
