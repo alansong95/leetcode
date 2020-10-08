4f5ecc58a50488eb4766eb2b743d02e30f67b45d
Alan Song <alansong95@gmail.com>
2020-10-07 20:27:41 -0700

    1089_duplicate_zeros: implement in place brute force

--- 1089_duplicate_zeros/duplicate_zeros.py
+++ 1089_duplicate_zeros/duplicate_zeros.py
@@ -1,17 +1,42 @@
 # if constraint "in place" is not there
+# time: O(n)
+# space: O(n)
+# class Solution(object):
+#     def duplicateZeros(self, arr):
+#         """
+#         :type arr: List[int]
+#         :rtype: None Do not return anything, modify arr in-place instead.
+#         """
+#         dest = []
+#         for e in arr:
+#             dest.append(e)
+#             if e == 0:
+#                 dest.append(e)
+#         arr = dest[:len(arr)]
+#         print(arr)
+
+
+# if constraint "in place" is there
+# brute force
 class Solution(object):
     def duplicateZeros(self, arr):
         """
         :type arr: List[int]
         :rtype: None Do not return anything, modify arr in-place instead.
         """
-        dest = []
-        for e in arr:
-            dest.append(e)
-            if e == 0:
-                dest.append(e)
-        arr = dest[:len(arr)]
+        i = 0
+        while i < len(arr): 
+            if arr[i] == 0:
+                temp_prev = 0
+                for j in range(i+1, len(arr)):
+                    if j < len(arr):
+                        temp = arr[j]
+                        arr[j] = temp_prev
+                        temp_prev = temp
+                i+=1
+            i+=1
         print(arr)
+
             
             
 
