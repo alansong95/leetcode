cf9c873a92348a88ab8edc3fbad8c6ba24d9ce42
Alan Song <alansong95@gmail.com>
2020-10-07 18:09:17 -0700

    977_squares_of_a_sorted_array: implement one line solution

--- 977_squares_of_a_sorted_array/squares_of_a_sorted_array.py
+++ 977_squares_of_a_sorted_array/squares_of_a_sorted_array.py
@@ -1,15 +1,27 @@
 # Brute force
 # Time: O(nlogn)
 # Space: 1
+# class Solution(object):
+#     def sortedSquares(self, A):
+#         """
+#         :type A: List[int]
+#         :rtype: List[int]
+#         """
+#         for i in range(len(A)):
+#             A[i] = A[i] * A[i]
+#         return sorted(A)
+
+# one line solution
 class Solution(object):
     def sortedSquares(self, A):
         """
         :type A: List[int]
         :rtype: List[int]
         """
-        for i in range(len(A)):
-            A[i] = A[i] * A[i]
-        return sorted(A)
+        # for i in range(len(A)):
+        #     A[i] = A[i] * A[i]
+        # return sorted(A)
+        return sorted([x * x for x in A])
 
 solution = Solution()
 print(solution.sortedSquares([-4,-1,0,3,10]))
