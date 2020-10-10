bd73d3787f2f2c3a546dcb2612ac101b524bda88
Alan Song <alansong95@gmail.com>
2020-10-10 11:26:29 -0700

    27_remove_element: implement using list comprehension

--- 27_remove_element/remove_element.py
+++ 27_remove_element/remove_element.py
@@ -1,6 +1,22 @@
 # remove method
 # Time: O(n)
 # Space: O(1)
+# class Solution(object):
+#     def removeElement(self, nums, val):
+#         """
+#         :type nums: List[int]
+#         :type val: int
+#         :rtype: int
+#         """
+#         try:
+#             while True:
+#                 nums.remove(val)
+#         except:
+#             print(nums)
+
+# List comprehension 
+# Time: O(n)
+# Space: O(1)
 class Solution(object):
     def removeElement(self, nums, val):
         """
@@ -8,11 +24,8 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        try:
-            while True:
-                nums.remove(val)
-        except:
-            print(nums)
+        nums[:] = [n for n in nums if n != val]
+        print(nums)
 
 solution = Solution()
 solution.removeElement([3,2,2,3], 3)
