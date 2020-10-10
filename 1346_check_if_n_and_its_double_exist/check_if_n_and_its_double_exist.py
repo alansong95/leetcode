36a924329f97e0c6f93edf1caa2422356f4ae502
Alan Song <alansong95@gmail.com>
2020-10-10 14:02:48 -0700

    1346_check_if_n_and_its_double_exist: implement using hash table

--- 1346_check_if_n_and_its_double_exist/check_if_n_and_its_double_exist.py
+++ 1346_check_if_n_and_its_double_exist/check_if_n_and_its_double_exist.py
@@ -1,20 +1,36 @@
 # brute force
 # Time: O(n^2)
 # Space: O(1)
+# class Solution(object):
+#     def checkIfExist(self, arr):
+#         """
+#         :type arr: List[int]
+#         :rtype: bool
+#         """
+#         if not arr or len(arr) == 0:
+#             return False
 
+#         for i in range(len(arr)):
+#             for j in range(i+1, len(arr)):
+#                 if arr[i] == arr[j] * 2 or arr[i] * 2 == arr[j]:
+#                     return True
+#         return False
+
+
+# use hash table
+# Time: O(n)
+# Space: O(n)
 class Solution(object):
     def checkIfExist(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: bool
-        """
         if not arr or len(arr) == 0:
             return False
-            
+
+        ht = {}
         for i in range(len(arr)):
-            for j in range(i+1, len(arr)):
-                if arr[i] == arr[j] * 2 or arr[i] * 2 == arr[j]:
-                    return True
+            if arr[i]*2 in ht or arr[i] / 2 in ht:
+                return True
+            else:
+                ht[arr[i]] = i
         return False
 
 solution = Solution()
@@ -23,3 +39,4 @@ print(solution.checkIfExist([7,1,14,11]))
 print(solution.checkIfExist([3,1,7,11]))
 print(solution.checkIfExist(None))
 print(solution.checkIfExist([]))
+print(solution.checkIfExist([3,1,7,11]))
