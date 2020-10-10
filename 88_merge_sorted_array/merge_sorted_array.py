746618704af013d5a11ba5e9696397cbbb502fa5
Alan Song <alansong95@gmail.com>
2020-10-10 10:51:41 -0700

    88_merge_sorted_array: implement space O(1) solution

--- 88_merge_sorted_array/merge_sorted_array.py
+++ 88_merge_sorted_array/merge_sorted_array.py
@@ -16,29 +16,54 @@
 # no sorting
 # Time: O(m+n)
 # Space: O(m)
+# class Solution(object):
+#     def merge(self, nums1, m, nums2, n):
+#         nums1_copy = nums1[:]
+#         nums1[:] = []
+#         p1 = 0
+#         p2 = 0
+#         while p1 < m and p2 < n:
+#             if nums1_copy[p1] < nums2[p2]:
+#                 nums1.append(nums1_copy[p1])
+#                 p1 += 1
+#             else:
+#                 nums1.append(nums2[p2])
+#                 p2 += 1
+        
+#         while p1 < m:
+#             nums1.append(nums1_copy[p1])
+#             p1 += 1
+#         while p2 < n:
+#             nums1.append(nums2[p2])
+#             p2 += 1
+        
+#         print(nums1)
+
+# no space solution
+# Time: O(m+n)
+# Space: O(1)
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        nums1_copy = nums1[:]
-        nums1[:] = []
-        p1 = 0
-        p2 = 0
-        while p1 < m and p2 < n:
-            if nums1_copy[p1] < nums2[p2]:
-                nums1.append(nums1_copy[p1])
-                p1 += 1
+        p = m + n - 1
+        p1 = m - 1
+        p2 = n - 1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
             else:
-                nums1.append(nums2[p2])
-                p2 += 1
-        
-        while p1 < m:
-            nums1.append(nums1_copy[p1])
-            p1 += 1
-        while p2 < n:
-            nums1.append(nums2[p2])
-            p2 += 1
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+
+        if p1 >= 0:
+            nums1[:p+1] = nums1[:p1+1] 
+        if p2 >= 0:
+            nums1[:p+1] = nums2[:p2+1]
         
         print(nums1)
 
 solution = Solution()
 solution.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
 solution.merge([1], 1, [], 0)
+solution.merge([0], 0, [1], 1)
