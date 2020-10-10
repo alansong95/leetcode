07cbbeb6dce30fd26dfb19b580374c42934337a0
Alan Song <alansong95@gmail.com>
2020-10-10 11:36:11 -0700

    27_remove_element: implement naive way

--- 27_remove_element/remove_element.py
+++ 27_remove_element/remove_element.py
@@ -40,5 +40,22 @@
 #         nums[:] = list(filter(lambda x: x != val, nums))
 #         print(nums)
 
+# naive way
+# Time: O(n)
+# Space: O(1)
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums:val: int
+        :rty List[int]
+        :type pe: int
+        """
+        p = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[p] = nums[i]
+                p += 1
+        return p
+
 solution = Solution()
 solution.removeElement([3,2,2,3], 3)
