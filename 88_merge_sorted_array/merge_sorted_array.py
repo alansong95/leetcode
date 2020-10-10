f49c6f6941e4ec6b401c1fad6bbd281f89a808f7
Alan Song <alansong95@gmail.com>
2020-10-10 10:34:36 -0700

    88_merge_sorted_array: implement using no sorting

--- 88_merge_sorted_array/merge_sorted_array.py
+++ 88_merge_sorted_array/merge_sorted_array.py
@@ -1,26 +1,44 @@
 # lazy solution
-# Time: O(nlogn)
+# Time: O((n+m)log(n+m))
 # Space: O(1)
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
-        nums1[len(nums1)-n:] = nums2
-        nums1[:] = sorted(nums1)[:]
-
-
 # class Solution(object):
 #     def merge(self, nums1, m, nums2, n):
-#         nums1_copy = nums1[:]
-#         p1 = 0
-#         p2 = 0
-#         while p1 < m and p2 < n:
+#         """
+#         :type nums1: List[int]
+#         :type m: int
+#         :type nums2: List[int]
+#         :type n: int
+#         :rtype: None Do not return anything, modify nums1 in-place instead.
+#         """
+#         nums1[len(nums1)-n:] = nums2
+#         nums1[:] = sorted(nums1)
 
+# no sorting
+# Time: O(m+n)
+# Space: O(m)
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        nums1_copy = nums1[:]
+        nums1[:] = []
+        p1 = 0
+        p2 = 0
+        while p1 < m and p2 < n:
+            if nums1_copy[p1] < nums2[p2]:
+                nums1.append(nums1_copy[p1])
+                p1 += 1
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+        
+        while p1 < m:
+            nums1.append(nums1_copy[p1])
+            p1 += 1
+        while p2 < n:
+            nums1.append(nums2[p2])
+            p2 += 1
+        
+        print(nums1)
 
 solution = Solution()
 solution.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
+solution.merge([1], 1, [], 0)
