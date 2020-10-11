a7e9d8a98ecc354bd5387d65709c8b1219b28947
Alan Song <alansong95@gmail.com>
2020-10-11 16:06:27 -0700

    complete: 283_move_zeroes

--- 283_move_zeroes/move_zeroes.py
+++ 283_move_zeroes/move_zeroes.py
@@ -1,13 +1,27 @@
-# brute force
-# Time: O(n^2)
-# Space: O(1)
+# count zeros
+# Time: O(n)
+# Space: O(n)
 class Solution:
     def moveZeroes(self, nums):
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                for j in range(i, len(nums)-1):
-                    nums[j+1], nums[j] = nums[j], nums[j+1]
+        ans = []
+
+        zero_count = 0
+        for num in nums:
+            if num == 0:
+                zero_count += 1
+            
+        for num in nums:
+            if num != 0:
+                ans.append(num)
+        
+        for i in range(zero_count):
+            ans.append(0)
+        
+        nums[:] = ans
         print(nums)
+
+        
 
 solution = Solution()
 print(solution.moveZeroes([0,1,0,3,12]))
+print(solution.moveZeroes([0,0,1]))
