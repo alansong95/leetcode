76248773e028c2abba9e8caa1d0a90d1213bc11c
Alan Song <alansong95@gmail.com>
2020-10-11 12:22:35 -0700

    complete: 1299_replace_elements_with_greatest_element_on_right_side

--- name.py
+++ name.py
@@ -11,6 +11,8 @@ file_name = '_'.join(name_list[1:])
 file_name += '.py'
 
 os.makedirs(dir_name)
-open(dir_name + '/' + file_name, 'a').close()
+f = open(dir_name + '/' + file_name, 'a')
+f.write('solution = Solution()\nprint(solution.')
+f.close()
 
 print('file: ' + file_name + ' created successfully.')
