1bbb2cc7148620814fc35edec3df830a9f9aaa87
Alan Song <alansong95@gmail.com>
2020-10-11 16:26:27 -0700

    283_move_zeroes.py: implement constant space

--- 283_move_zeroes.py
+++ 283_move_zeroes.py
@@ -1,26 +1,44 @@
 # count zeros
 # Time: O(n)
 # Space: O(n)
+# class Solution:
+#     def moveZeroes(self, nums):
+#         ans = []
+
+#         zero_count = 0
+#         for num in nums:
+#             if num == 0:
+#                 zero_count += 1
+            
+#         for num in nums:
+#             if num != 0:
+#                 ans.append(num)
+        
+#         for _ in range(zero_count):
+#             ans.append(0)
+        
+#         nums[:] = ans
+#         print(nums)
+
+# Constant space
+# Time: O(n)
+# Space: O(1)
 class Solution:
     def moveZeroes(self, nums):
-        ans = []
-
         zero_count = 0
         for num in nums:
             if num == 0:
                 zero_count += 1
             
-        for num in nums:
-            if num != 0:
-                ans.append(num)
-        
-        for i in range(zero_count):
-            ans.append(0)
-        
-        nums[:] = ans
-        print(nums)
-
-        
+        i = 0
+        while i < len(nums) - zero_count:
+            if nums[i] == 0:
+                j = i + 1
+                while nums[j] == 0:
+                    j += 1
+                nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+        print(nums)        
 
 solution = Solution()
 print(solution.moveZeroes([0,1,0,3,12]))
---
+++ 905_sort_array_by_parity.py
@@ -0,0 +1,15 @@
+# two lists 
+# Time: O(n)
+# Space: O(n)
+class Solution(object):
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        even = [x for x in A if x % 2 == 0]
+        odd = [x for x in A if x % 2 == 1]
+        return even + odd
+
+solution = Solution()
+print(solution.sortArrayByParity([3,1,2,4]))
