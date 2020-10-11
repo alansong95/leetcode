1f922c0817536333a2746c59b070c40874516627
Alan Song <alansong95@gmail.com>
2020-10-11 16:49:44 -0700

    905_sort_array_by_parity.py: implement in place

--- 905_sort_array_by_parity.py
+++ 905_sort_array_by_parity.py
@@ -1,15 +1,32 @@
 # two lists 
 # Time: O(n)
 # Space: O(n)
+# class Solution(object):
+#     def sortArrayByParity(self, A):
+#         """
+#         :type A: List[int]
+#         :rtype: List[int]
+#         """
+#         even = [x for x in A if x % 2 == 0]
+#         odd = [x for x in A if x % 2 == 1]
+#         return even + odd
+
+# in place
 class Solution(object):
     def sortArrayByParity(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
-        even = [x for x in A if x % 2 == 0]
-        odd = [x for x in A if x % 2 == 1]
-        return even + odd
+        i = 0
+        j = len(A) - 1
+        while i < j:
+            if A[i] % 2 == 0:
+                i += 1
+            else:
+                if A[j] % 2 == 1:
+                    j -= 1
+                else:
+                    A[j], A[i] = A[i], A[j]
+                    i += 1
+                    j -= 1
+        return A
 
 
 solution = Solution()
