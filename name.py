b1c2d421ea255177623cacdf9c4920aee3bb3fbc
Alan Song <alansong95@gmail.com>
2020-10-11 16:12:26 -0700

    change file structures

--- name.py
+++ name.py
@@ -6,12 +6,10 @@ name = ' '.join(name)
 name = name.replace('.', '').lower()
 name_list = name.split(' ')
 
-dir_name = '_'.join(name_list)
-file_name = '_'.join(name_list[1:])
+file_name = '_'.join(name_list)
 file_name += '.py'
 
-os.makedirs(dir_name)
-f = open(dir_name + '/' + file_name, 'a')
+f = open(file_name, 'a')
 f.write('solution = Solution()\nprint(solution.')
 f.close()
 
