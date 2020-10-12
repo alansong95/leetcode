5a2f03c4fe0c525204fcea8269fe6d96b8b82547
Alan Song <alansong95@gmail.com>
2020-10-11 17:38:01 -0700

    448_find_all_numbers_disappeared_in_an_array.py: implement linear time solution

--- 448_find_all_numbers_disappeared_in_an_array.py
+++ 448_find_all_numbers_disappeared_in_an_array.py
@@ -1,12 +1,28 @@
 # lazy
 # Time: O(nlogn)
 # Space: O(n)
-class Solution:
-    def findDisappearedNumbers(self, nums):
-        nums2 = set(range(1, len(nums)+1))
-        nums = set(nums)
-        return sorted(list(nums2 - nums))
+# class Solution:
+#     def findDisappearedNumbers(self, nums):
+#         nums2 = set(range(1, len(nums)+1))
+#         nums = set(nums)
+#         return sorted(list(nums2 - nums))
 
+        
+# linear time solution
+# Time: O(n)
+# Space: O(n)
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        ht = {}
+        for num in nums:
+            ht[num] = num
+        
+        ans = []
+        for i in range(1, len(nums) + 1):
+            if i not in ht:
+                ans.append(i)
+        return ans
+        
         
 
 solution = Solution()
