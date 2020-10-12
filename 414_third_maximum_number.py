05a097c8041210a9dfc22a2d006640b47150a8ba
Alan Song <alansong95@gmail.com>
2020-10-11 17:21:22 -0700

    414_third_maximum_number.py: implement linear time solution

--- 414_third_maximum_number.py
+++ 414_third_maximum_number.py
@@ -2,17 +2,39 @@
 # lazy solution
 # Time: O(nlogn)
 # Space: O(n)
+# class Solution:
+#     def thirdMax(self, nums):
+#         if not nums:
+#             return None
+
+#         nums = sorted(list(set(nums)))
+#         ind = len(nums) - 3
+#         if ind < 0:
+#             return nums[-1]
+#         return nums[ind]
+
+# linear time solution
+# Time: O(n)
 class Solution:
     def thirdMax(self, nums):
         if not nums:
             return None
             
-        nums = sorted(list(set(nums)))
+        nums = set(nums)
+        max1 = max(nums)
+
+        if len(nums) < 3:
+            return max1
+        
+        nums.remove(max1)
+        max2 = max(nums)
+        nums.remove(max2)
+        return max(nums)
+
         ind = len(nums) - 3
         if ind < 0:
             return nums[-1]
         return nums[ind]
-
         
 solution = Solution()
 print(solution.thirdMax([3,2,1]))
