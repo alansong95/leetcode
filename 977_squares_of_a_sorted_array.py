f8d28b0732d3b8ec4327e4663dba9e52b5f6c899
Alan Song <alansong95@gmail.com>
2020-10-11 17:00:40 -0700

    977_squares_of_a_sorted_array.py: implement linear solution

--- 977_squares_of_a_sorted_array.py
+++ 977_squares_of_a_sorted_array.py
@@ -12,16 +12,32 @@
 #         return sorted(A)
 
 # one line solution
+# Time: O(nlogn)
+# Space: n
+# class Solution(object):
+#     def sortedSquares(self, A):
+#         return sorted([x * x for x in A])
+
+# linear solution
+# Time: O(n)
+# Space: O(n)
 class Solution(object):
     def sortedSquares(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
-        # for i in range(len(A)):
-        #     A[i] = A[i] * A[i]
-        # return sorted(A)
-        return sorted([x * x for x in A])
+        ans = [0] * len(A)
+
+        left = 0
+        right = len(A) - 1
+
+        for i in range(len(ans)-1, -1, -1):
+            if abs(A[left]) > abs(A[right]):
+                ans[i] = A[left] * A[left]
+                left += 1
+            else:
+                ans[i] = A[right] * A[right]
+                right -= 1
+        return ans
+
+
 
 solution = Solution()
 print(solution.sortedSquares([-4,-1,0,3,10]))
