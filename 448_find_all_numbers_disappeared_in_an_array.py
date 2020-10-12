fbb6f7c1bcdd1cb70c6f623d786520cf266831f9
Alan Song <alansong95@gmail.com>
2020-10-11 17:44:39 -0700

    448_find_all_numbers_disappeared_in_an_array.py: implement linear time const space

--- 448_find_all_numbers_disappeared_in_an_array.py
+++ 448_find_all_numbers_disappeared_in_an_array.py
@@ -11,19 +11,33 @@
 # linear time solution
 # Time: O(n)
 # Space: O(n)
+# class Solution(object):
+#     def findDisappearedNumbers(self, nums):
+#         ht = {}
+#         for num in nums:
+#             ht[num] = num
+        
+#         ans = []
+#         for i in range(1, len(nums) + 1):
+#             if i not in ht:
+#                 ans.append(i)
+#         return ans
+        
+        
+# linear time const space solution
+# Time: O(n)
+# Space: O(1)
 class Solution(object):
     def findDisappearedNumbers(self, nums):
-        ht = {}
-        for num in nums:
-            ht[num] = num
-        
         ans = []
-        for i in range(1, len(nums) + 1):
-            if i not in ht:
-                ans.append(i)
+
+        for i in range(len(nums)):
+            nums[abs(nums[i])-1] = -1 * abs(nums[abs(nums[i])-1])
+        
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                ans.append(i+1)
         return ans
-        
-        
 
 solution = Solution()
 print(solution.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
