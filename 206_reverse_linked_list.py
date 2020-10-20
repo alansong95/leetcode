83a14b14d32d77fe7d6f70205f9a1fdcba2155f0
Alan Song <alansong95@gmail.com>
2020-10-19 20:19:56 -0700

    206_reverse_linked_list.py: implement using recursion

--- 206_reverse_linked_list.py
+++ 206_reverse_linked_list.py
@@ -1,19 +1,37 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        if head:
-            ptr = head
-            while ptr.next:
-                temp = ptr.next
-                ptr.next = temp.next
-                temp.next = head
-                head = temp
-            return head
+# # Definition for singly-linked list.
+# # class ListNode(object):
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution(object):
+#     def reverseList(self, head):
+#         """
+#         :type head: ListNode
+#         :rtype: ListNode
+#         """
+#         if head:
+#             ptr = head
+#             while ptr.next:
+#                 temp = ptr.next
+#                 ptr.next = temp.next
+#                 temp.next = head
+#                 head = temp
+#             return head
+
+# # Definition for singly-linked list.
+# # class ListNode(object):
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution(object):
+#     def reverseList(self, head):
+#         """
+#         :type head: ListNode
+#         :rtype: ListNode
+#         """
+#         if not head or not head.next:
+#             return head
+#         h = self.reverseList(head.next)
+#         head.next.next = head
+#         head.next = None
+#         return h
