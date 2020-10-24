7dc6da5ebc0ac12673083806df67507ace127407
Alan Song <alansong95@gmail.com>
2020-10-24 13:52:28 -0700

    58_length_of_last_word.py: implement using loop

--- 58_length_of_last_word.py
+++ 58_length_of_last_word.py
@@ -1,9 +1,27 @@
+# class Solution(object):
+#     def lengthOfLastWord(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         if len(s.split()) == 0:
+#             return 0
+#         return len(s.split()[-1])
+
 class Solution(object):
     def lengthOfLastWord(self, s):
         """
         :type s: str
         :rtype: int
         """
-        if len(s.split()) == 0:
-            return 0
-        return len(s.split()[-1])
+        p = len(s)-1
+        
+        while p >= 0 and s[p] == ' ':
+            p -= 1
+        
+        count = 0
+        while p >= 0 and s[p] != ' ':
+            count += 1
+            p -= 1
+        return count
+        
