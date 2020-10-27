fee687705531735f05cad4758d6fcfd2e05bbc14
Alan Song <alansong95@gmail.com>
2020-10-27 14:25:32 -0700

    70_climbing_stairs.py: implement using memoization

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -1,19 +1,30 @@
 # brute force
 # time:  O(2^n)
 # space: O(n)
+# class Solution(object):
+#     def climbStairs(self, n):
+#         if n < 0:
+#             return 0
+#         elif n <= 2:
+#             return n
+#         else:
+#             return self.climbStairs(n-2) + self.climbStairs(n-1)
+
+# 
 class Solution(object):
+    memo = {-2: 0, -1: 0, 0: 0, 1: 1, 2: 2}
+
     def climbStairs(self, n):
-        if n < 0:
-            return 0
-        elif n <= 2:
-            return n
+        if n in self.memo:
+            return self.memo[n]
         else:
-            return self.climbStairs(n-2) + self.climbStairs(n-1)
-        
+            temp = self.climbStairs(n-2) + self.climbStairs(n-1)
+            self.memo[n] = temp
+            return temp
 
 solution = Solution()
 print(solution.climbStairs(1) == 1)
 print(solution.climbStairs(2) == 2)
 print(solution.climbStairs(3) == 3)
 print(solution.climbStairs(20) == 10946)
-# print(solution.climbStairs(45) == 1836311903)
+print(solution.climbStairs(45) == 1836311903)
