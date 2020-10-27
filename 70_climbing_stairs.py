8bd564bc95689579a920a512f92327d4093debb6
Alan Song <alansong95@gmail.com>
2020-10-27 14:33:11 -0700

    70_climbing_stairs.py: implement using constant space

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -10,21 +10,39 @@
 #         else:
 #             return self.climbStairs(n-2) + self.climbStairs(n-1)
 
-# 
-class Solution(object):
-    memo = {-2: 0, -1: 0, 0: 0, 1: 1, 2: 2}
+# memoization
+# time: O(n)
+# space: O(n)
+# class Solution(object):
+#     memo = {-2: 0, -1: 0, 0: 0, 1: 1, 2: 2}
 
+#     def climbStairs(self, n):
+#         if n in self.memo:
+#             return self.memo[n]
+#         else:
+#             temp = self.climbStairs(n-2) + self.climbStairs(n-1)
+#             self.memo[n] = temp
+#             return temp
+
+class Solution(object):
     def climbStairs(self, n):
-        if n in self.memo:
-            return self.memo[n]
-        else:
-            temp = self.climbStairs(n-2) + self.climbStairs(n-1)
-            self.memo[n] = temp
-            return temp
+        f, s = 1, 2
+        for _ in range(n-1):
+            f, s = s, f + s
+        return f
+
+
 
 solution = Solution()
 print(solution.climbStairs(1) == 1)
 print(solution.climbStairs(2) == 2)
 print(solution.climbStairs(3) == 3)
+print(solution.climbStairs(4) == 5)
 print(solution.climbStairs(20) == 10946)
 print(solution.climbStairs(45) == 1836311903)
+
+print(solution.climbStairs(2))
+print(solution.climbStairs(3))
+print(solution.climbStairs(4))
+print(solution.climbStairs(20))
+print(solution.climbStairs(45))
