3d36e424bb4a059b21b0325227a70951c7b554ae
Alan Song <alansong95@gmail.com>
2020-10-29 21:33:44 -0700

    125_valid_palindrome.py: implement using two pointers

--- 125_valid_palindrome.py
+++ 125_valid_palindrome.py
@@ -1,14 +1,31 @@
 # string manipulation
 # time: O(n)
 # space: 1
+# class Solution(object):
+#     def isPalindrome(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         s = (''.join(x for x in s if x.isalnum())).lower()
+#         return s == s[::-1]
+
+# two pointers
+# time: O(n)
+# space: 1
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s = (''.join(x for x in s if x.isalnum())).lower()
-        return s == s[::-1]
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
 
 solution = Solution()
 print(solution.isPalindrome('A man, a plan, a canal: Panama'))
