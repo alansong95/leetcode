46aea327af062e9cce8a73e6e9f54da7bb8f07a0
Alan Song <alansong95@gmail.com>
2020-10-30 16:58:37 -0700

    136_single_number.py: use set to achieve space: O(1)

--- 136_single_number.py
+++ 136_single_number.py
@@ -1,16 +1,23 @@
 # hash table
 # time: O(n)
 # space: O(n)
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         table = {}
+#         for num in nums:
+#             if num not in table:
+#                 table[num] = 1
+#             else:
+#                 del table[num]
+#         return table.keys()[0]
+
+# use sets
+# time: O(n)
+# space: O(1)
 class Solution(object):
     def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        table = {}
-        for num in nums:
-            if num not in table:
-                table[num] = 1
-            else:
-                del table[num]
-        return table.keys()[0]
+        return 2 * sum(set(nums)) - sum(nums)
