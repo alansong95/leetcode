536d6a920b96267c9e0d39fadcc2a10ddec37e50
Alan Song <alansong95@gmail.com>
2020-11-01 10:11:22 -0800

    167_two_sum_ii_-_input_array_is_sorted.py: implement using two pointers

--- 167_two_sum_ii_-_input_array_is_sorted.py
+++ 167_two_sum_ii_-_input_array_is_sorted.py
@@ -18,15 +18,31 @@
 # hash table
 # time: O(n)
 # space: O(n)
+# class Solution(object):
+#     def twoSum(self, numbers, target):
+#         table = {}
+        
+#         for i, num in enumerate(numbers):
+#             if (target - num) in table:
+#                 return [table[target-num]+1, i+1]
+#             if num not in table:
+#                 table[num] = i
+
+# two pointers
+# time: O(n)
+# space: O(1)
 class Solution(object):
     def twoSum(self, numbers, target):
-        table = {}
-        
-        for i, num in enumerate(numbers):
-            if (target - num) in table:
-                return [table[target-num]+1, i+1]
-            if num not in table:
-                table[num] = i
+        l, r = 0, len(numbers) - 1
+        while l <= r:
+            _sum = numbers[l] + numbers[r]
+            if _sum == target:
+                return [l+1, r+1]
+            elif _sum > target:
+                r -= 1
+            else:
+                l += 1
+
 
 solution = Solution()
 print(solution.
