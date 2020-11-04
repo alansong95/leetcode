baf685678333c396e47374699373cc1d8a8e24c5
Alan Song <alansong95@gmail.com>
2020-11-03 17:14:16 -0800

    1480_running_sum_of_1d_array.py: implement using itertools

--- 1480_running_sum_of_1d_array.py
+++ 1480_running_sum_of_1d_array.py
@@ -1,18 +1,24 @@
 # one pass
 # time: O(n)
 # space: O(1)
+# class Solution(object):
+#     def runningSum(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[int]
+#         """
+#         total = 0
+#         runningSum = []
+#         for num in nums:
+#             total += num
+#             runningSum.append(total)
+#         return runningSum
+
+# python accumulate
+from itertools import accumulate 
 class Solution(object):
     def runningSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        total = 0
-        runningSum = []
-        for num in nums:
-            total += num
-            runningSum.append(total)
-        return runningSum
+        return list(accumulate(nums))
 
 solution = Solution()
 print(solution.runningSum([1,2,3,4]))
