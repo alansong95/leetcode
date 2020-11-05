32f5d5242b774a4c679b1861d476fa655e39af0b
Alan Song <alansong95@gmail.com>
2020-11-04 18:07:33 -0800

    1470_shuffle_the_array.py: implement more clean code

--- 1470_shuffle_the_array.py
+++ 1470_shuffle_the_array.py
@@ -1,3 +1,18 @@
+# class Solution(object):
+#     def shuffle(self, nums, n):
+#         """
+#         :type nums: List[int]
+#         :type n: int
+#         :rtype: List[int]
+#         """
+#         first = nums[0:n]
+#         second = nums[n:]
+#         output = []
+#         for i in range(n):
+#             output.append(first[i])
+#             output.append(second[i])
+#         return output
+
 class Solution(object):
     def shuffle(self, nums, n):
         """
@@ -5,10 +20,8 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        first = nums[0:n]
-        second = nums[n:]
         output = []
         for i in range(n):
-            output.append(first[i])
-            output.append(second[i])
+            output.append(nums[i])
+            output.append(nums[n+i])
         return output
