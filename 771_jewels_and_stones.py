406c238a8ed2997cbe8f602d82ee0836624438a8
Alan Song <alansong95@gmail.com>
2020-11-06 11:43:19 -0800

    771_jewels_and_stones.py: implement with one line

--- 771_jewels_and_stones.py
+++ 771_jewels_and_stones.py
@@ -1,20 +1,25 @@
 # hash table
 # time: O(J + S)
 # space: O(J)
+# class Solution(object):
+#     def numJewelsInStones(self, J, S):
+#         """
+#         :type J: str
+#         :type S: str
+#         :rtype: int
+#         """
+#         ht = {}
+#         count = 0
+#         for j in J:
+#             if j not in ht:
+#                 ht[j] = 1
+        
+#         for s in S:
+#             if s in ht:
+#                 count += 1
+#         return count
+
+# one line answer
 class Solution(object):
     def numJewelsInStones(self, J, S):
-        """
-        :type J: str
-        :type S: str
-        :rtype: int
-        """
-        ht = {}
-        count = 0
-        for j in J:
-            if j not in ht:
-                ht[j] = 1
-        
-        for s in S:
-            if s in ht:
-                count += 1
-        return count
+        return len([s for s in S if s in J])
