f2bb70527e44d94782647ffb41b4ebbd9c21ec44
Alan Song <alansong95@gmail.com>
2020-11-08 11:52:54 -0800

    complete: 1342_number_of_steps_to_reduce_a_number_to_zero.py

---
+++ 1342_number_of_steps_to_reduce_a_number_to_zero.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def numberOfSteps (self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num > 0:
+            
+
+solution = Solution()
+print(solution.
