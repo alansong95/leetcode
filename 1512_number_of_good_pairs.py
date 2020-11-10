fdab5a304b0c848a7bcf16819ed27e26c0b3af57
Alan Song <alansong95@gmail.com>
2020-11-09 18:34:49 -0800

    1512_number_of_good_pairs.py: implement using hash table

--- 1512_number_of_good_pairs.py
+++ 1512_number_of_good_pairs.py
@@ -1,3 +1,22 @@
+# brute force
+# time: O(n^2)
+# space: O(1)
+# class Solution(object):
+#     def numIdenticalPairs(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         count = 0
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] == nums[j]:
+#                     count += 1
+#         return count
+
+# hash table
+# time: O(n)
+# space: O(n)
 class Solution(object):
     def numIdenticalPairs(self, nums):
         """
@@ -5,8 +24,14 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j]:
-                    count += 1
+        ht = {}
+        for i, num in enumerate(nums):
+            if num in ht:
+                ht[num] += 1
+            else:
+                ht[num] = 1
+        
+        _values = ht.values()
+        for value in _values:
+            count += (value*(value-1))/2
         return count
