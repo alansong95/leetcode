9f70b95cffa812e3d0892b2ca2ab0a65c549d21d
Alan Song <alansong95@gmail.com>
2020-11-13 12:07:13 -0800

    1491_average_salary_excluding_the_minimum_and_maximum_salary.py: implement linear solution

--- 1491_average_salary_excluding_the_minimum_and_maximum_salary.py
+++ 1491_average_salary_excluding_the_minimum_and_maximum_salary.py
@@ -1,12 +1,18 @@
 # sorting
 # time: O(nlogn)
 # space: O(1)
+# class Solution(object):
+#     def average(self, salary):
+#         """
+#         :type salary: List[int]
+#         :rtype: float
+#         """
+#         salarySorted = sorted(salary)
+#         return sum(salarySorted[1:len(salarySorted)-1]) / float((len(salarySorted)-2))
+            
+# linear solution
+# time: O(n)
+# space: O(1)
 class Solution(object):
     def average(self, salary):
-        """
-        :type salary: List[int]
-        :rtype: float
-        """
-        salarySorted = sorted(salary)
-        return sum(salarySorted[1:len(salarySorted)-1]) / float((len(salarySorted)-2))
-            
+        return (sum(salary) - max(salary) - min(salary)) / float(len(salary) - 2)
