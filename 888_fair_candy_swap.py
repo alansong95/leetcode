3cbcc06a6e8eeb4bcd9d6824291c2581f4833a4e
Alan Song <alansong95@gmail.com>
2020-11-12 16:20:27 -0800

    888_fair_candy_swap.py: use set

--- 888_fair_candy_swap.py
+++ 888_fair_candy_swap.py
@@ -1,3 +1,22 @@
+# class Solution(object):
+#     def fairCandySwap(self, A, B):
+#         """
+#         :type A: List[int]
+#         :type B: List[int]
+#         :rtype: List[int]
+#         """
+#         sumA = sum(A)
+#         sumB = sum(B)
+#         diff = (sumA - sumB) / 2
+        
+#         ht = {}
+#         for a in A:
+#             ht[a] = a
+        
+#         for b in B:
+#             if (b + diff) in ht:
+#                 return [ht[b+diff], b]
+
 class Solution(object):
     def fairCandySwap(self, A, B):
         """
@@ -9,10 +28,10 @@ class Solution(object):
         sumB = sum(B)
         diff = (sumA - sumB) / 2
         
-        ht = {}
+        _set = set()
         for a in A:
-            ht[a] = a
+            _set.add(a)
         
         for b in B:
-            if (b + diff) in ht:
-                return [ht[b+diff], b]
+            if (b + diff) in _set:
+                return [b+diff, b]
