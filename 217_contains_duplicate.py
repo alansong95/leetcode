31a26082269f06cd8fa161f35ba124615d65bafc
Alan Song <alansong95@gmail.com>
2020-11-12 17:31:47 -0800

    217_contains_duplicate.py: implement using sorting

--- 217_contains_duplicate.py
+++ 217_contains_duplicate.py
@@ -1,4 +1,19 @@
-import statistics
-class Solution:
-    def containsDuplicate(self, nums) -> bool:
-        return len(statistics.multimode(nums)) != len(nums)
+# import statistics
+# class Solution:
+#     def containsDuplicate(self, nums) -> bool:
+#         return len(statistics.multimode(nums)) != len(nums)
+
+# implement using sorting
+# time: O(nlogn)
+# space: O(n)
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        sortedNums = sorted(nums)
+        for i in range(1, len(sortedNums)):
+            if sortedNums[i-1] == sortedNums[i]:
+                return True
+        return False
