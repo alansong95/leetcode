bff19df6eb03afabc37653b0b59fb5375e3e556d
Alan Song <alansong95@gmail.com>
2020-11-12 17:33:54 -0800

    217_contains_duplicate.py: implement using hash table

--- 217_contains_duplicate.py
+++ 217_contains_duplicate.py
@@ -6,14 +6,30 @@
 # implement using sorting
 # time: O(nlogn)
 # space: O(n)
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         sortedNums = sorted(nums)
+#         for i in range(1, len(sortedNums)):
+#             if sortedNums[i-1] == sortedNums[i]:
+#                 return True
+#         return False
+
+# implement using hash table
+# time: O(n)
+# space: O(n)
 class Solution(object):
     def containsDuplicate(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        sortedNums = sorted(nums)
-        for i in range(1, len(sortedNums)):
-            if sortedNums[i-1] == sortedNums[i]:
+        ht = {}
+        for num in nums:
+            if num in ht:
                 return True
+            ht[num] = num
         return False
