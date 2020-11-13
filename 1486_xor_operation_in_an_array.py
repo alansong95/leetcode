0990de308936b40e2e0ba1decadaec34b7961bec
Alan Song <alansong95@gmail.com>
2020-11-13 11:52:45 -0800

    1486_xor_operation_in_an_array.py: one pass and constant space

--- 1486_xor_operation_in_an_array.py
+++ 1486_xor_operation_in_an_array.py
@@ -1,16 +1,29 @@
+# two passes and a list
+# time: O(n)
+# space: O(n)
+# class Solution(object):
+#     def xorOperation(self, n, start):
+#         """
+#         :type n: int
+#         :type start: int
+#         :rtype: int
+#         """
+#         nums = []
+#         for i in range(n):
+#             nums.append(start + 2*i)
+        
+#         xored = nums[0]
+#         for num in nums[1:]:
+#             xored = xored ^ num
+#         return xored
+        
+# one pass and no list
+# time: O(n)
+# space: O(1)
 class Solution(object):
     def xorOperation(self, n, start):
-        """
-        :type n: int
-        :type start: int
-        :rtype: int
-        """
-        nums = []
-        for i in range(n):
-            nums.append(start + 2*i)
-        
-        xored = nums[0]
-        for num in nums[1:]:
-            xored = xored ^ num
+        xored = start
+        for i in range(1, n):
+            xored = xored ^ (start + 2*i)
         return xored
         
