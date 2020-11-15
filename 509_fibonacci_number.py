8d638aae53cd0df9c2043783fd57095af542f0de
Alan Song <alansong95@gmail.com>
2020-11-15 14:07:32 -0800

    implement iteratively

--- 509_fibonacci_number.py
+++ 509_fibonacci_number.py
@@ -1,11 +1,23 @@
-# recursive
+# # recursive
+# class Solution(object):
+#     def fib(self, N):
+#         """
+#         :type N: int
+#         :rtype: int
+#         """
+#         if N == 0 or N == 1:
+#             return N
+#         else:
+#             return self.fib(N - 1) + self.fib(N - 2)
+
+# iterative
 class Solution(object):
     def fib(self, N):
         """
         :type N: int
         :rtype: int
         """
-        if N == 0 or N == 1:
-            return N
-        else:
-            return self.fib(N - 1) + self.fib(N - 2)
+        v1, v2 = 0, 1
+        for i in range(N):
+            v2, v1 = v1 + v2, v2
+        return v1
