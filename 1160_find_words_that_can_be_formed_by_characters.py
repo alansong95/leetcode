4d859bb58d244c8e91d1c6b3af1be133e665b9af
Alan Song <alansong95@gmail.com>
2020-11-15 14:02:05 -0800

    1160_find_words_that_can_be_formed_by_characters.py: implement using hash table

--- 1160_find_words_that_can_be_formed_by_characters.py
+++ 1160_find_words_that_can_be_formed_by_characters.py
@@ -1,3 +1,20 @@
+# import collections
+# class Solution(object):
+#     def countCharacters(self, words, chars):
+#         """
+#         :type words: List[str]
+#         :type chars: str
+#         :rtype: int
+#         """
+#         output = 0
+#         charsCounter = collections.Counter(chars)
+#         for word in words:
+#             wordCounter = collections.Counter(word)
+#             if (charsCounter & wordCounter) == wordCounter:
+#                 output += len(word)
+#         return output
+
+# using hash table
 class Solution(object):
     def countCharacters(self, words, chars):
         """
@@ -6,10 +23,23 @@ class Solution(object):
         :rtype: int
         """
         output = 0
-        charsCounter = collections.Counter(chars)
+        ht = {}
+        
+        for c in chars:
+            ht[c] = ht.get(c, 0) + 1
+        
+        addFlag = 1
         for word in words:
-            wordCounter = collections.Counter(word)
-            if (charsCounter & wordCounter) == wordCounter:
+            addFlag = 1
+            tempHt = ht.copy()
+            for c in word:
+                if c in tempHt and tempHt[c] > 0:
+                    tempHt[c] -= 1
+                else:
+                    addFlag = 0
+                    break
+            if addFlag == 1:
                 output += len(word)
+            
         return output
             
