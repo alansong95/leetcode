0e3ee722ad544887bbd8a2ef142b2764fa6b7e99
Alan Song <alansong95@gmail.com>
2020-11-15 14:04:54 -0800

    complete: 509_fibonacci_number.py

---
+++ 509_fibonacci_number.py
@@ -0,0 +1,11 @@
+# recursive
+class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if N == 0 or N == 1:
+            return N
+        else:
+            return self.fib(N - 1) + self.fib(N - 2)
