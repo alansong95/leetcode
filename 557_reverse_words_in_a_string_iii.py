a832b16a6bf3a465996f46d3df685c648159a6c4
Alan Song <alansong95@gmail.com>
2020-11-17 20:07:46 -0800

    557_reverse_words_in_a_string_iii.py: make it simpler

--- 557_reverse_words_in_a_string_iii.py
+++ 557_reverse_words_in_a_string_iii.py
@@ -1,10 +1,19 @@
+# class Solution(object):
+#     def reverseWords(self, s):
+#         """
+#         :type s: str
+#         :rtype: str
+#         """
+#         splited = s.split(' ')
+#         for i in range(len(splited)):
+#             splited[i] = splited[i][::-1]
+#         return ' '.join(splited)
+
 class Solution(object):
     def reverseWords(self, s):
         """
         :type s: str
         :rtype: str
         """
-        splited = s.split(' ')
-        for i in range(len(splited)):
-            splited[i] = splited[i][::-1]
+        splited = [w[::-1] for w in s.split(' ')]
         return ' '.join(splited)
