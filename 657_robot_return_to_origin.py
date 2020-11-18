8a7fa84d18b46a2c1a4706c43a88c203e4aec1c6
Alan Song <alansong95@gmail.com>
2020-11-17 20:00:27 -0800

    complete: 657_robot_return_to_origin.py

---
+++ 657_robot_return_to_origin.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        counter = collections.Counter(moves)
+        return counter.get('U') == counter.get('D') and counter.get('R') == counter.get('L')
