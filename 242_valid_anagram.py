4079d8b364a4fae7c8bd995fe93c08e401680c80
Alan Song <alansong95@gmail.com>
2020-11-19 17:55:32 -0800

    242_valid_anagram.py: implement more clean way

--- 242_valid_anagram.py
+++ 242_valid_anagram.py
@@ -1,4 +1,15 @@
 import collections
+# class Solution(object):
+#     def isAnagram(self, s, t):
+#         """
+#         :type s: str
+#         :type t: str
+#         :rtype: bool
+#         """
+#         counterS = collections.Counter(s)
+#         counterT = collections.Counter(t)
+#         return len((counterS - counterT).keys()) == 0 and len((counterT - counterS).keys()) == 0
+
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -6,6 +17,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        counterS = collections.Counter(s)
-        counterT = collections.Counter(t)
-        return len((counterS - counterT).keys()) == 0 and len((counterT - counterS).keys()) == 0
+        return collections.Counter(s) == collections.Counter(t)
