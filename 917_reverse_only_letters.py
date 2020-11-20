50df11c317ae30b5608bf31c6631eed46a9472fe
Alan Song <alansong95@gmail.com>
2020-11-19 17:47:11 -0800

    complete: 389_find_the_difference.py

--- 917_reverse_only_letters.py
+++ 917_reverse_only_letters.py
@@ -10,10 +10,12 @@ class Solution(object):
         r = len(S) - 1
         
         while l < r:
-            while not S[l].isalpha():
+            while l < r and not S[l].isalpha():
                 l += 1
-            while not S[r].isalpha():
+            while r > l and not S[r].isalpha():
                 r -= 1
+            if l >= r:
+                break
             S[l], S[r] = S[r], S[l]
             l += 1
             r -= 1
