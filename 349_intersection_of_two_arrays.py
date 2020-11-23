5d8d0699bb338c70c9504bdd668d4770a0bcf5b0
Alan Song <alansong95@gmail.com>
2020-11-23 15:01:48 -0800

    349_intersection_of_two_arrays.py: use set intersection

--- 349_intersection_of_two_arrays.py
+++ 349_intersection_of_two_arrays.py
@@ -1,3 +1,12 @@
+# class Solution(object):
+#     def intersection(self, nums1, nums2):
+#         """
+#         :type nums1: List[int]
+#         :type nums2: List[int]
+#         :rtype: List[int]
+#         """
+#         return collections.Counter(nums1) & collections.Counter(nums2)
+
 class Solution(object):
     def intersection(self, nums1, nums2):
         """
@@ -5,4 +14,4 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        return collections.Counter(nums1) & collections.Counter(nums2)
+        return set(nums1).intersection(set(nums2))
