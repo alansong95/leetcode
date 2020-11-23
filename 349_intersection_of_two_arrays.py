8f17ca61e6ca5d35d6baa598f510b2c8f9d8f636
Alan Song <alansong95@gmail.com>
2020-11-23 15:03:32 -0800

    349_intersection_of_two_arrays.py: implement without using intersection method

--- 349_intersection_of_two_arrays.py
+++ 349_intersection_of_two_arrays.py
@@ -7,6 +7,15 @@
 #         """
 #         return collections.Counter(nums1) & collections.Counter(nums2)
 
+# class Solution(object):
+#     def intersection(self, nums1, nums2):
+#         """
+#         :type nums1: List[int]
+#         :type nums2: List[int]
+#         :rtype: List[int]
+#         """
+#         return set(nums1).intersection(set(nums2))
+
 class Solution(object):
     def intersection(self, nums1, nums2):
         """
@@ -14,4 +23,10 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        return set(nums1).intersection(set(nums2))
+        set1 = set(nums1)
+        set2 = set()
+        
+        for num in nums2:
+            if num in set1:
+                set2.add(num)
+        return list(set2)
