38967822bc5bc4b9f4bdba4964483c9d13132c6f
Alan Song <alansong95@gmail.com>
2020-11-25 14:49:08 -0800

    complete: 409_longest_palindrome.py

---
+++ 409_longest_palindrome.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        flag = 0
+        output = 0
+        counter = collections.Counter(s).values()
+        for c in counter:
+            while c >= 2:
+                output += 2
+                c -= 2
+            if flag == 0 and c % 2 == 1:
+                output += 1
+                flag = 1
+            
+        return output
