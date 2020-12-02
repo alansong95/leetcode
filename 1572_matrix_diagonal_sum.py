2fb1434637ff9090fff36ab9bfcba227d9069abe
Alan Song <alansong95@gmail.com>
2020-12-02 08:47:28 -0800

    1572_matrix_diagonal_sum.py: implement more clean solution

--- 1572_matrix_diagonal_sum.py
+++ 1572_matrix_diagonal_sum.py
@@ -1,17 +1,32 @@
+# class Solution(object):
+#     def diagonalSum(self, mat):
+#         """
+#         :type mat: List[List[int]]
+#         :rtype: int
+#         """
+#         i = 0
+#         j = len(mat[0]) - 1
+#         _sum = 0    
+        
+#         while i < len(mat[0]):
+#             if i != j:
+#                 _sum += mat[i][i]
+#             _sum += mat[i][j]
+#             i += 1
+#             j -= 1
+#         return _sum
+
 class Solution(object):
     def diagonalSum(self, mat):
         """
         :type mat: List[List[int]]
         :rtype: int
         """
-        i = 0
-        j = len(mat[0]) - 1
-        _sum = 0    
+        n = len(mat)
+        mid = n // 2
+        res = 0
         
-        while i < len(mat[0]):
-            if i != j:
-                _sum += mat[i][i]
-            _sum += mat[i][j]
-            i += 1
-            j -= 1
-        return _sum
+        for i in range(n):
+            res += mat[i][i] +mat[i][n-i-1]
+        
+        return res - mat[mid][mid] if n & 1 else res
