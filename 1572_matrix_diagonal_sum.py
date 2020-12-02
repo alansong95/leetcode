0c8e6d31b6b43d5288ca79506ea512f164cd5d73
Alan Song <alansong95@gmail.com>
2020-12-02 08:42:29 -0800

    1572_matrix_diagonal_sum.py: clean up code

--- 1572_matrix_diagonal_sum.py
+++ 1572_matrix_diagonal_sum.py
@@ -4,17 +4,14 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: int
         """
-        k = 0
         i = 0
         j = len(mat[0]) - 1
+        _sum = 0    
         
-        _sum = 0
-        
-        while j >= 0:
+        while i < len(mat[0]):
             if i != j:
-                _sum += mat[k][i]
-            _sum += mat[k][j]
+                _sum += mat[i][i]
+            _sum += mat[i][j]
             i += 1
             j -= 1
-            k += 1
         return _sum
