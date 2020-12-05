0d9c40d9ab3014999f5680e3ce0dec08fcddc9d7
Alan Song <alansong95@gmail.com>
2020-12-04 23:22:58 -0800

    1550_three_consecutive_odds.py: use string

--- 1550_three_consecutive_odds.py
+++ 1550_three_consecutive_odds.py
@@ -1,15 +1,23 @@
+# class Solution(object):
+#     def threeConsecutiveOdds(self, arr):
+#         """
+#         :type arr: List[int]
+#         :rtype: bool
+#         """
+#         count = 0
+#         for n in arr:
+#             if n % 2 == 1:
+#                 count += 1
+#             else:
+#                 count = 0
+#             if count == 3:
+#                 return True
+#         return False
+
 class Solution(object):
     def threeConsecutiveOdds(self, arr):
         """
         :type arr: List[int]
         :rtype: bool
         """
-        count = 0
-        for n in arr:
-            if n % 2 == 1:
-                count += 1
-            else:
-                count = 0
-            if count == 3:
-                return True
-        return False
+        return '111' in ''.join([str(x % 2) for x in arr])
