0050d8fcb42846e5bee0e81fccc2dd4b0a09519b
Alan Song <alansong95@gmail.com>
2020-12-06 20:11:21 -0800

    896_monotonic_array.py: implement one liner

--- 896_monotonic_array.py
+++ 896_monotonic_array.py
@@ -22,19 +22,27 @@
 #                 return False
 #         return True
 
+# class Solution(object):
+#     def isMonotonic(self, A):
+#         """
+#         :type A: List[int]
+#         :rtype: bool
+#         """
+#         increased = False
+#         decreased = False
+        
+#         for i in range(0, len(A)-1):
+#             if A[i] < A[i+1]:
+#                 increased = True
+#             elif A[i] > A[i+1]:
+#                 decreased = True
+        
+#         return False if increased and decreased else True
+
 class Solution(object):
     def isMonotonic(self, A):
         """
         :type A: List[int]
         :rtype: bool
         """
-        increased = False
-        decreased = False
-        
-        for i in range(0, len(A)-1):
-            if A[i] < A[i+1]:
-                increased = True
-            elif A[i] > A[i+1]:
-                decreased = True
-        
-        return False if increased and decreased else True
+        return not {cmp(i,j) for i, j in zip(A, A[1:])} >= {1, -1}
