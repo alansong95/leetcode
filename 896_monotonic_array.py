43916aaf17751037e2c05bbc8cd71c54f0071bac
Alan Song <alansong95@gmail.com>
2020-12-06 20:07:41 -0800

    896_monotonic_array.py: implement more clean solution

--- 896_monotonic_array.py
+++ 896_monotonic_array.py
@@ -1,23 +1,40 @@
+# class Solution(object):
+#     def isMonotonic(self, A):
+#         """
+#         :type A: List[int]
+#         :rtype: bool
+#         """
+#         _dir = 0
+        
+#         ind = 0
+#         while _dir == 0 and ind < len(A) - 1:
+#             if A[ind] < A[ind+1]:
+#                 _dir = 1
+#             elif A[ind] > A[ind+1]:
+#                 _dir = -1
+#             ind += 1
+            
+#         if _dir == 0:
+#             return True
+            
+#         for i in range(ind, len(A) - 1):
+#             if A[i+1] - A[i] != 0 and (A[i+1] - A[i]) * _dir < 0:
+#                 return False
+#         return True
+
 class Solution(object):
     def isMonotonic(self, A):
         """
         :type A: List[int]
         :rtype: bool
         """
-        _dir = 0
+        increased = False
+        decreased = False
         
-        ind = 0
-        while _dir == 0 and ind < len(A) - 1:
-            if A[ind] < A[ind+1]:
-                _dir = 1
-            elif A[ind] > A[ind+1]:
-                _dir = -1
-            ind += 1
-            
-        if _dir == 0:
-            return True
-            
-        for i in range(ind, len(A) - 1):
-            if A[i+1] - A[i] != 0 and (A[i+1] - A[i]) * _dir < 0:
-                return False
-        return True
+        for i in range(0, len(A)-1):
+            if A[i] < A[i+1]:
+                increased = True
+            elif A[i] > A[i+1]:
+                decreased = True
+        
+        return False if increased and decreased else True
