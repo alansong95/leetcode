53d7fe633394c0c94527dfeadd6d6a459440c3ac
Alan Song <alansong95@gmail.com>
2020-12-08 19:55:34 -0800

    1678_goal_parser_interpretation.py: implement using string

--- 1678_goal_parser_interpretation.py
+++ 1678_goal_parser_interpretation.py
@@ -1,23 +1,27 @@
+# class Solution(object):
+#     def interpret(self, command):
+#         """
+#         :type command: str
+#         :rtype: str
+#         """
+#         output = ""
+#         stack = []
+#         for c in command:
+#             if c == 'G':
+#                 output += 'G'
+#             elif c == '(':
+#                 stack.append(c)
+#             elif c == ')':
+#                 new_c = ''.join(stack) + ')'
+#                 if new_c == '()':
+#                     output += 'o'
+#                 elif new_c == '(al)':
+#                     output += 'al'
+#                 stack = []
+#             else:
+#                 stack.append(c)
+#         return output
+
 class Solution(object):
     def interpret(self, command):
-        """
-        :type command: str
-        :rtype: str
-        """
-        output = ""
-        stack = []
-        for c in command:
-            if c == 'G':
-                output += 'G'
-            elif c == '(':
-                stack.append(c)
-            elif c == ')':
-                new_c = ''.join(stack) + ')'
-                if new_c == '()':
-                    output += 'o'
-                elif new_c == '(al)':
-                    output += 'al'
-                stack = []
-            else:
-                stack.append(c)
-        return output
+        return command.replace('()', 'o').replace('(al)', 'al')
