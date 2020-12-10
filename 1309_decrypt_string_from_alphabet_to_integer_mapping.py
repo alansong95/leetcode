63000a7d01a0e89bdfe9765f51bc1471b6aaf178
Alan Song <alansong95@gmail.com>
2020-12-09 16:21:04 -0800

    1309_decrypt_string_from_alphabet_to_integer_mapping.py: implement using regex

--- 1309_decrypt_string_from_alphabet_to_integer_mapping.py
+++ 1309_decrypt_string_from_alphabet_to_integer_mapping.py
@@ -1,18 +1,22 @@
+# class Solution(object):
+#     def freqAlphabets(self, s):
+#         """
+#         :type s: str
+#         :rtype: str
+#         """
+#         # 10
+#         output = ''
+#         i = 0
+#         while i < len(s):
+#             if i+2 < len(s) and s[i+2] == '#':
+#                 output += chr(int(s[i] + s[i+1]) + 96)
+#                 i += 3
+#             else:
+#                 output += chr(int(s[i]) + 96)
+#                 i += 1
+#         return output
+
 class Solution(object):
     def freqAlphabets(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        # 10
-        output = ''
-        i = 0
-        while i < len(s):
-            if i+2 < len(s) and s[i+2] == '#':
-                output += chr(int(s[i] + s[i+1]) + 96)
-                i += 3
-            else:
-                output += chr(int(s[i]) + 96)
-                i += 1
-        return output
-                
+        return ''.join([chr(int(x[:2])+96) for x in re.findall(r'\d\d#|\d', s)])
+
