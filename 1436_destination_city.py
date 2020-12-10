8f1ba3619d40ab897818e9356d20511538c00fea
Alan Song <alansong95@gmail.com>
2020-12-09 16:28:02 -0800

    1436_destination_city.py: implement using set difference

--- 1436_destination_city.py
+++ 1436_destination_city.py
@@ -1,14 +1,23 @@
+# class Solution(object):
+#     def destCity(self, paths):
+#         """
+#         :type paths: List[List[str]]
+#         :rtype: str
+#         """
+#         ht = {}
+#         for path in paths:
+#             ht[path[0]] = path[1]
+        
+#         city = paths[0][0]
+#         while city in ht:
+#             city = ht[city]
+#         return city
+
 class Solution(object):
     def destCity(self, paths):
         """
         :type paths: List[List[str]]
         :rtype: str
         """
-        ht = {}
-        for path in paths:
-            ht[path[0]] = path[1]
-        
-        city = paths[0][0]
-        while city in ht:
-            city = ht[city]
-        return city
+        start, end = map(set, zip(*paths))
+        return (end - start).pop()
