b054af6978af4697e60865ec93ce9365793f4514
Alan Song <alansong95@gmail.com>
2020-12-10 19:01:20 -0800

    929_unique_email_addresses.py: search for + only once

--- 929_unique_email_addresses.py
+++ 929_unique_email_addresses.py
@@ -1,3 +1,18 @@
+# class Solution(object):
+#     def numUniqueEmails(self, emails):
+#         """
+#         :type emails: List[str]
+#         :rtype: int
+#         """
+#         unique_emails = set()
+#         for email in emails:
+#             local, domain = email.split('@')
+#             if '+' in local:
+#                 local = local[0:local.index('+')]
+#             local = local.replace('.', '') + '@' + domain
+#             unique_emails.add(local)
+#         return len(unique_emails)
+
 class Solution(object):
     def numUniqueEmails(self, emails):
         """
@@ -7,8 +22,6 @@ class Solution(object):
         unique_emails = set()
         for email in emails:
             local, domain = email.split('@')
-            if '+' in local:
-                local = local[0:local.index('+')]
-            local = local.replace('.', '') + '@' + domain
-            unique_emails.add(local)
+            local = local.split('+')[0].replace('.', '')
+            unique_emails.add((local, domain))
         return len(unique_emails)
