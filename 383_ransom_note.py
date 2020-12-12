a5eb1882b146136a8fb84e4688c926cc04ef666c
Alan Song <alansong95@gmail.com>
2020-12-12 11:12:16 -0800

    383_ransom_note.py: implement more efficient solution

--- 383_ransom_note.py
+++ 383_ransom_note.py
@@ -1,4 +1,13 @@
-import collections
+# import collections
+# class Solution(object):
+#     def canConstruct(self, ransomNote, magazine):
+#         """
+#         :type ransomNote: str
+#         :type magazine: str
+#         :rtype: bool
+#         """
+#         return len(collections.Counter(ransomNote) - collections.Counter(magazine))== 0
+
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
         """
@@ -6,4 +15,7 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
-        return len(collections.Counter(ransomNote) - collections.Counter(magazine))== 0
+        for c in set(ransomNote):
+            if ransomNote.count(c) > magazine.count(c):
+                return False
+        return True
