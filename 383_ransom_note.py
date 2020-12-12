35db60bfe542efb7b5ba5cb1ec2239a24b24a88d
Alan Song <alansong95@gmail.com>
2020-12-12 11:06:58 -0800

    complete: 383_ransom_note.py

---
+++ 383_ransom_note.py
@@ -0,0 +1,9 @@
+import collections
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        return len(collections.Counter(ransomNote) - collections.Counter(magazine))== 0
