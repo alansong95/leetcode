f0fa6c2c96ad7c6d7e14863bd467243381602cd3
Alan Song <alansong95@gmail.com>
2020-12-12 10:56:31 -0800

    345_reverse_vowels_of_a_string.py: implement using two pointers

--- 345_reverse_vowels_of_a_string.py
+++ 345_reverse_vowels_of_a_string.py
@@ -1,18 +1,36 @@
+# class Solution(object):
+#     def reverseVowels(self, s):
+#         """
+#         :type s: str
+#         :rtype: str
+#         """
+#         vowels =  ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+#         s = list(s)
+#         rev_vowels = [c for c in s[::-1] if c in vowels]
+        
+#         rev_i = 0
+#         for i, c in enumerate(s):
+#             if c in vowels:
+#                 s[i] = rev_vowels[rev_i]
+#                 rev_i += 1
+#         return ''.join(s)
+        
+        
 class Solution(object):
     def reverseVowels(self, s):
         """
         :type s: str
         :rtype: str
         """
-        vowels =  ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        vowels = set('aeiouAEIOU')
         s = list(s)
-        rev_vowels = [c for c in s[::-1] if c in vowels]
         
-        rev_i = 0
-        for i, c in enumerate(s):
-            if c in vowels:
-                s[i] = rev_vowels[rev_i]
-                rev_i += 1
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and s[l] not in vowels: l += 1
+            while l < r and s[r] not in vowels: r -= 1
+            if l > r: break
+            s[l], s[r] = s[r], s[l]
+            l, r = l + 1, r -1
         return ''.join(s)
-        
         
