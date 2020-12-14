02a54cd426fb9b167240bc8806b5766af0058af7
Alan Song <alansong95@gmail.com>
2020-12-13 22:59:00 -0800

    415_add_strings.py: implement without uisng int

--- 415_add_strings.py
+++ 415_add_strings.py
@@ -1,3 +1,12 @@
+# class Solution(object):
+#     def addStrings(self, num1, num2):
+#         """
+#         :type num1: str
+#         :type num2: str
+#         :rtype: str
+#         """
+#         return str(int(num1) + int(num2))
+
 class Solution(object):
     def addStrings(self, num1, num2):
         """
@@ -5,4 +14,22 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        return str(int(num1) + int(num2))
+        res = ''
+        prev_carry = 0
+        carry = 0
+
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        while len(num1) < len(num2): num1 += '0'
+        while len(num2) < len(num1): num2 += '0'
+            
+        
+        for n1, n2 in zip(num1, num2):
+            _sum = (ord(n1) - ord('0')) + (ord(n2) - ord('0'))
+            carry = (prev_carry + _sum) // 10
+            _sum = (prev_carry + _sum) % 10
+            res += str(_sum)
+            prev_carry = carry
+        if carry == 1:
+            res += '1'
+        return res[::-1]
