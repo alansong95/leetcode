27a5d3a06419638c478563738ef92656b0f14692
Alan Song <alansong95@gmail.com>
2020-12-15 18:46:56 -0800

    989_add_to_array-form_of_integer.py: implement without converting int str

--- 989_add_to_array-form_of_integer.py
+++ 989_add_to_array-form_of_integer.py
@@ -1,3 +1,12 @@
+# class Solution(object):
+#     def addToArrayForm(self, A, K):
+#         """
+#         :type A: List[int]
+#         :type K: int
+#         :rtype: List[int]
+#         """
+#         return [int(c) for c in str(int(''.join([str(a) for a in A])) + K)]
+
 class Solution(object):
     def addToArrayForm(self, A, K):
         """
@@ -5,4 +14,11 @@ class Solution(object):
         :type K: int
         :rtype: List[int]
         """
-        return [int(c) for c in str(int(''.join([str(a) for a in A])) + K)]
+        for i in range (len(A) -1, -1, -1):
+            if not K: break
+            K, A[i] = divmod(K + A[i], 10)
+        while K:
+            K, a = divmod(K, 10)
+            A = [a] + A
+        return A
+            
