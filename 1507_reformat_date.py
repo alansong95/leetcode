9f4de952b975b8973a8d89b515eda3dc164be431
Alan Song <alansong95@gmail.com>
2020-12-18 07:39:10 -0800

    complete: 1507_reformat_date.py

--- 1507_reformat_date.py
+++ 1507_reformat_date.py
@@ -1,4 +1,14 @@
-
-
-solution = Solution()
-print(solution.
+class Solution(object):
+    def reformatDate(self, date):
+        """
+        :type date: str
+        :rtype: str
+        """
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        splitted = date.split(' ')
+        day = splitted[0][:-2]
+        month = str(months.index(splitted[1]) + 1)
+        year = splitted[2]
+        month = month if len(month) == 2 else '0' + month
+        day = day if len(day) == 2 else '0' + day
+        return year + '-' + month + '-' + day
