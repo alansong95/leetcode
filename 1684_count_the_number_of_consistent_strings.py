6f5e553c7f5da0908b31882d6a964f9c3360203f
Alan Song <alansong95@gmail.com>
2020-12-21 10:30:18 -0800

    complete: 1684_count_the_number_of_consistent_strings.py

---
+++ 1684_count_the_number_of_consistent_strings.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countConsistentStrings(self, allowed, words):
+        """
+        :type allowed: str
+        :type words: List[str]
+        :rtype: int
+        """
+        return sum([all(c in allowed for c in w) for w in words])
