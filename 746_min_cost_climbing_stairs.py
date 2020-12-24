2bd98ec7769ac99495e3c97e2b6fad6f63ce0231
Alan Song <alansong95@gmail.com>
2020-12-24 13:29:44 -0800

    746_min_cost_climbing_stairs.py: implement using constant space

--- 746_min_cost_climbing_stairs.py
+++ 746_min_cost_climbing_stairs.py
@@ -15,18 +15,31 @@
 #         print dp
 #         return dp[n-1]
 
+# class Solution(object):
+#     def minCostClimbingStairs(self, p):
+#         """
+#         :type cost: List[int]
+#         :rtype: int
+#         """
+#         n = len(p)
+#         dp = [0] * n
+#         dp[0] = p[0]
+#         dp[1] = p[1]
+        
+#         for i in range(2, n):
+#             dp[i] = min(dp[i-1], dp[i-2]) + p[i]
+            
+#         return min(dp[-1], dp[-2])
+
 class Solution(object):
     def minCostClimbingStairs(self, p):
         """
         :type cost: List[int]
         :rtype: int
         """
-        n = len(p)
-        dp = [0] * n
-        dp[0] = p[0]
-        dp[1] = p[1]
+        a, b = p[0], p[1]
         
-        for i in range(2, n):
-            dp[i] = min(dp[i-1], dp[i-2]) + p[i]
-            
-        return min(dp[-1], dp[-2])
+        for i in range(2, len(p)):
+            c = min(a, b) + p[i]
+            a, b = b, c
+        return min(a, b)
