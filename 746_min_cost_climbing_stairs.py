d4ff9021f34dd2d7c62a70976ef8bbe45dca1bf9
Alan Song <alansong95@gmail.com>
2020-12-24 13:26:21 -0800

    746_min_cost_climbing_stairs.py: clean up the code

--- 746_min_cost_climbing_stairs.py
+++ 746_min_cost_climbing_stairs.py
@@ -1,10 +1,26 @@
+# class Solution(object):
+#     def minCostClimbingStairs(self, p):
+#         """
+#         :type cost: List[int]
+#         :rtype: int
+#         """
+#         p = p + [0]
+#         n = len(p)
+#         dp = [0] * n
+#         dp[0] = p[0]
+#         dp[1] = p[1]
+        
+#         for i in range(2, n):
+#             dp[i] = min(dp[i-1], dp[i-2]) + p[i]
+#         print dp
+#         return dp[n-1]
+
 class Solution(object):
     def minCostClimbingStairs(self, p):
         """
         :type cost: List[int]
         :rtype: int
         """
-        p = p + [0]
         n = len(p)
         dp = [0] * n
         dp[0] = p[0]
@@ -12,5 +28,5 @@ class Solution(object):
         
         for i in range(2, n):
             dp[i] = min(dp[i-1], dp[i-2]) + p[i]
-        print dp
-        return dp[n-1]
+            
+        return min(dp[-1], dp[-2])
