5e0622d1032b08a7fea81662607e182dd8d4019a
Alan Song <alansong95@gmail.com>
2020-12-27 11:11:22 -0800

    509_fibonacci_number.py: implement using backward bottom up dynamic programming

--- 509_fibonacci_number.py
+++ 509_fibonacci_number.py
@@ -45,7 +45,7 @@ class Solution2(object):
         memo[n] = self.fibHelper(n-1, memo) + self.fibHelper(n-2, memo)
         return memo[n]
 
-# bottom up dynamic programming
+# bottom up dynamic programming (forward)
 class Solution3(object):
     def fib(self, n):
         if n < 2:
@@ -57,4 +57,18 @@ class Solution3(object):
         for i in range(2, n+1):
             dp[i] = dp[i-1] + dp[i-2]
 
+        return dp[n]
+
+# bottom up dynamic programming (backward)
+class Solution4(object):
+    def fib(self, n):
+        if n < 2:
+            return n
+        
+        dp = [0] * (n+2)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(1, n):
+            dp[i+1] += dp[i]
+            dp[i+2] += dp[i]
         return dp[n]
