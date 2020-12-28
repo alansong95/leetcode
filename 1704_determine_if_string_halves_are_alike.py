c65cffc9d45d964de04e265ac9e358421cdf5c76
Alan Song <alansong95@gmail.com>
2020-12-28 10:33:38 -0800

    1704_determine_if_string_halves_are_alike.py: use set for better performance

--- 1704_determine_if_string_halves_are_alike.py
+++ 1704_determine_if_string_halves_are_alike.py
@@ -14,13 +14,29 @@
 #                 count2 += 1
 #         return count1 == count2
         
+# class Solution(object):
+#     def halvesAreAlike(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+#         count = 0
+#         for i in range(len(s)/2):
+#             if s[i] in vowels:
+#                 count += 1
+#         for i in range(len(s)/2, len(s)):
+#             if s[i] in vowels:
+#                 count -= 1
+#         return count == 0
+        
 class Solution(object):
     def halvesAreAlike(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
         count = 0
         for i in range(len(s)/2):
             if s[i] in vowels:
