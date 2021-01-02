8dcc8b342efe16e2cf59cd1461a0976991450724
Alan Song <alansong95@gmail.com>
2021-01-02 10:01:31 -0800

    94_binary_tree_inorder_traversal.py: implement more clean version

--- 94_binary_tree_inorder_traversal.py
+++ 94_binary_tree_inorder_traversal.py
@@ -20,3 +20,21 @@ class Solution(object):
         self.output = []
         helper(root)
         return self.output
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution2(object):
+    def inorderTraversal(self, root):
+        res = []
+        self.helper(root, res)
+        return res
+    
+    def helper(self, root, res):
+        if root:
+            self.helper(root.left, res)
+            res.append(root.val)
+            self.helper(root.right, res)
