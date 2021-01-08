69296979530c5e00614ebdcc9950474d861ab8d6
Alan Song <alansong95@gmail.com>
2021-01-08 09:27:49 -0800

    2_add_two_numbers.py: implement more clean code

--- 2_add_two_numbers.py
+++ 2_add_two_numbers.py
@@ -41,3 +41,34 @@ class Solution(object):
             res.next = ListNode(carry)
             
         return resCopy.next
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution2(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        res = ListNode()
+        resCopy = res
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = val2 = 0
+            if l1:
+                val1 = l1.val
+                l1 = l1.next
+            if l2:
+                val2 = l2.val
+                l2 = l2.next
+            carry, _sum = divmod(val1 + val2 + carry, 10)
+            res.next = ListNode(_sum)
+            res = res.next
+            
+        return resCopy.next
