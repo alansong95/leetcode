a6457c0191b0bdc7b0ebb0b0503b57583399c13f
Alan Song <alansong95@gmail.com>
2021-01-07 20:26:47 -0800

    3_longest_substring_without_repeating_characters.py: implement more clean solution

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -20,3 +20,22 @@ class Solution(object):
                 seen[s[i]] = i
                 
         return max(longest, len(s)-l)
+
+
+class Solution2(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        seen = {}
+        l = 0
+        longest = 0
+        
+        for i, c in enumerate(s):
+            if c in seen:
+                longest = max(longest, i - l)
+                l = max(l, seen[s[i]] + 1)
+            seen[s[i]] = i
+        
+        return max(longest, len(s)-l)
