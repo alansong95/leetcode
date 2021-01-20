3f141b12c39747d4fc0771bdd1f1f9f69d442816
Alan Song <alansong95@gmail.com>
2021-01-19 19:50:54 -0800

    5_longest_palindromic_substring.py: implement in n^2

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -1,3 +1,4 @@
+# time: O(n^3)
 class Solution(object):
     def longestPalindrome(self, s):
         """
@@ -12,3 +13,30 @@ class Solution(object):
     
     def isPalindrome(self, s):
         return s == s[::-1]
+
+# time: O(n^2)
+class Solution2(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        
+        ans = ''
+        for i in range(n):
+            dp[i][i] = True
+        ans = s[0]
+        
+        maxLen = 1
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n):
+                if s[i] == s[j]:
+                    if j - i == 1 or dp[i+1][j-1]:
+                        dp[i][j] = True
+                        if maxLen < j - i + 1:
+                            maxLen = j - i + 1
+                            ans = s[i:j+1]
+        return ans
+                
