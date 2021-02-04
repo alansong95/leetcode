57d84c9c01027710bcd6e4812a9f918effe330a9
Alan Song <alansong95@gmail.com>
2021-02-03 20:03:16 -0800

    20_valid_parentheses.py: implement using hash table

--- 141_linked_list_cycle.py
+++ 141_linked_list_cycle.py
@@ -21,4 +21,24 @@ class Solution(object):
             fast = fast.next.next
 
         return True
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution2(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        seen = set()
         
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        return False
