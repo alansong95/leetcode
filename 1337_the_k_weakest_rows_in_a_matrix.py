f62f936344567c7d82323e8fb8975035cce8fb25
Alan Song <alansong95@gmail.com>
2021-02-15 21:02:29 -0800

    1337_the_k_weakest_rows_in_a_matrix.py: implement using priority queue

--- 1337_the_k_weakest_rows_in_a_matrix.py
+++ 1337_the_k_weakest_rows_in_a_matrix.py
@@ -10,3 +10,24 @@ class Solution(object):
             ht[i] = sum(row)
 
         return [key for key, value in sorted(ht.items(), key=lambda x: x[1])][:k]
+
+
+from Queue import PriorityQueue
+
+class Solution2(object):
+    def kWeakestRows(self, mat, k):
+        """
+        :type mat: List[List[int]]
+        :type k: int
+        :rtype: List[int]
+        """
+        q = PriorityQueue()
+
+        for i, row in enumerate(mat):
+            q.put((sum(row), i))
+
+        res = []
+        for _ in range(k):
+            res.append(q.get()[1])
+            
+        return res
