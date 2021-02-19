aaa888f08eee0937279663b8c3f5f30800a15290
Alan Song <alansong95@gmail.com>
2021-02-18 18:01:13 -0800

    complete: 413_arithmetic_slices.py

---
+++ 413_arithmetic_slices.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        dp = [0] * len(A)
+        _sum = 0
+        
+        for i in range(2, len(A)):
+            if A[i] - A[i-1] == A[i-1] - A[i-2]:
+                dp[i] = 1 + dp[i-1]
+                _sum += dp[i]
+        print dp
+        return _sum
