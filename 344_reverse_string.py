13e0db77c7a6701790b537942a1f0df41a2d9dc5
Alan Song <alansong95@gmail.com>
2021-02-21 22:04:10 -0800

    344_reverse_string.py: implement using recursion

--- 344_reverse_string.py
+++ 344_reverse_string.py
@@ -10,5 +10,14 @@ class Solution(object):
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
+
+class Solution2(object):
+    def reverseString(self, s):
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left+1, right-1)
+        helper(0, len(s)-1)
+
             
     
