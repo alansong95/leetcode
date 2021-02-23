8e5a12e2e921e1384363b6ca722f468b5b010358
Alan Song <alansong95@gmail.com>
2021-02-22 21:49:52 -0800

    21_merge_two_sorted_lists.py: implement using recursion

--- 21_merge_two_sorted_lists.py
+++ 21_merge_two_sorted_lists.py
@@ -54,3 +54,22 @@ print(solution.printList(solution.mergeTwoLists(l1, l2)))
 print(solution.printList(solution.mergeTwoLists(l12, l22)))
 print(solution.printList(solution.mergeTwoLists(l13, l23)))
 print(solution.printList(solution.mergeTwoLists(l14, l24)))
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution2(object):
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+        
