f1f9876b9b20b6b4917face91e6b35469b3496be
Alan Song <alansong95@gmail.com>
2021-02-23 21:19:18 -0800

    complete: 200_number_of_islands.py

---
+++ 200_number_of_islands.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid or len(grid) == 0:
+            return 0
+        
+        nr = len(grid)
+        nc = len(grid[0])
+        num_islands = 0
+        
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c] == '1':
+                    num_islands += 1
+                    grid[r][c] = '0'
+                    neighbors = [(r, c)]
+                    while len(neighbors) > 0:
+                        row, col = neighbors.pop(0)
+                        
+                        if row-1 >= 0 and grid[row-1][col] == '1':
+                            neighbors.append((row-1, col))
+                            grid[row-1][col] = '0'
+                        if row+1 < nr and grid[row+1][col] == '1':
+                            neighbors.append((row+1, col))
+                            grid[row+1][col] = '0'
+                        if col-1 >= 0 and grid[row][col-1] == '1':
+                            neighbors.append((row, col-1))
+                            grid[row][col-1] = '0'
+                        if col+1 < nc and grid[row][col+1] == '1':
+                            neighbors.append((row, col+1))
+                            grid[row][col+1] = '0'
+        print grid
+        return num_islands
