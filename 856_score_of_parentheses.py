e91fd6e539c3fa488e0a186b4edb42526e76b0c8
Alan Song <alansong95@gmail.com>
2021-02-24 15:16:16 -0800

    complete: 856_score_of_parentheses.py

---
+++ 856_score_of_parentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def scoreOfParentheses(self, S):
+        """
+        :type S: str
+        :rtype: int
+        """
+        stack = [0]
+        
+        for x in S:
+            if x == '(':
+                stack.append(0)
+            else:
+                v = stack.pop()
+                stack[-1] += max(2*v, 1)
+        return stack.pop()
