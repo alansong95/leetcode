635cbcacfdd30cb278e96e4e45635d81c105df2d
Alan Song <alansong95@gmail.com>
2021-02-24 20:37:45 -0800

    841_keys_and_rooms.py: implement using dfs

--- 841_keys_and_rooms.py
+++ 841_keys_and_rooms.py
@@ -1,3 +1,4 @@
+# bfs
 class Solution(object):
     def canVisitAllRooms(self, rooms):
         """
@@ -5,7 +6,6 @@ class Solution(object):
         :rtype: bool
         """
         seen = set([0])
-        num_of_rooms = len(rooms)
         queue = [0]
         
         while queue:
@@ -17,5 +17,23 @@ class Solution(object):
             seen.add(i)
         
         return len(rooms) == len(seen)
-            
+
+# dfs
+class Solution2(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        seen = set([0])
+        stack = [0]
         
+        while stack:
+            i = stack.pop()
+            room = rooms[i]
+            for key in room:
+                if key not in seen:
+                    stack.append(key)
+            seen.add(i)
+        
+        return len(rooms) == len(seen)
