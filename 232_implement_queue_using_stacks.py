b4c4dda3f6f37e37092f9c57a4f33821588b00fd
Alan Song <alansong95@gmail.com>
2021-02-24 16:38:56 -0800

    232_implement_queue_using_stacks.py: improve the solution by using a variable: "first"

--- 232_implement_queue_using_stacks.py
+++ 232_implement_queue_using_stacks.py
@@ -63,3 +63,67 @@ class MyQueue(object):
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+
+class MyQueue2(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack1 = []
+        self.stack2 = []
+        self.first = None
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: None
+        """
+        if self.first is None:
+            self.first = x
+        self.stack1.append(x)
+        
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        popped = None
+        if self.stack1:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+            popped = self.stack2.pop()
+            if self.stack2:
+                self.first = self.stack2[-1]
+                while self.stack2:
+                    self.stack1.append(self.stack2.pop())
+            else:
+                self.first = None
+        return popped
+        
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        return self.first
+    
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        return len(self.stack1) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
