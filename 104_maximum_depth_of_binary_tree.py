8af29f868a7e75782bacbb5fc39020c0bcba9123
Alan Song <alansong95@gmail.com>
2021-02-24 22:51:40 -0800

    104_maximum_depth_of_binary_tree.py: implement using top-down

--- 104_maximum_depth_of_binary_tree.py
+++ 104_maximum_depth_of_binary_tree.py
@@ -14,3 +14,28 @@ class Solution(object):
             return 0
         
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+# top-down 
+class Solution2(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        
+        self.max_depth = 0
+        def helper(root, depth):
+            if root is None:
+                return 0
+            
+            if root.left is None and root.right is None:
+                self.max_depth = max(self.max_depth, depth)
+            
+            helper(root.left, depth+1)
+            helper(root.right, depth+1)
+            
+        helper(root, 1)
+        return self.max_depth
+        
