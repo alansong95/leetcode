4a3328308be8811f104fad5b24946067f6dfd797
Alan Song <alansong95@gmail.com>
2021-02-28 14:28:46 -0800

    895_maximum_frequency_stack.py: implement O(1) solution

--- 895_maximum_frequency_stack.py
+++ 895_maximum_frequency_stack.py
@@ -1,5 +1,7 @@
 # O(log n) heap
 import collections
+import heapq
+
 class FreqStack(object):
     def __init__(self):
         self.heap = []
@@ -24,7 +26,32 @@ class FreqStack(object):
         return removed[2]
 
 
-# Your FreqStack object will be instantiated and called as such:
-# obj = FreqStack()
-# obj.push(x)
-# param_2 = obj.pop()
+# O(1)
+class FreqStack2(object):
+    def __init__(self):
+        self.freq = collections.Counter()
+        self.group = collections.defaultdict(list)
+        self.maxfreq = 0
+        
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.freq[x] += 1
+        if self.freq[x] > self.maxfreq:
+            self.maxfreq = self.freq[x]
+        self.group[self.freq[x]].append(x)
+        
+        
+    def pop(self):
+        """
+        :rtype: int
+        """
+        x = self.group[self.maxfreq].pop()
+        self.freq[x] -= 1
+        if not self.group[self.maxfreq]:
+            self.maxfreq -= 1
+        
+        return x
+
