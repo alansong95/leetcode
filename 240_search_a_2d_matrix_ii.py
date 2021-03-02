4e4a3e9087392919a0c16dcdc6693f6f3fa65c27
Alan Song <alansong95@gmail.com>
2021-03-01 22:26:42 -0800

    240_search_a_2d_matrix_ii.py: implement using divide and conquer

--- 240_search_a_2d_matrix_ii.py
+++ 240_search_a_2d_matrix_ii.py
@@ -21,3 +21,33 @@ class Solution(object):
             if binarySearch(row, target):
                 return True
         return False
+
+
+# divide and conquer 
+class Solution2(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        
+        def search(left, up, right, down):
+            if left > right or up > down:
+                return False
+            elif target < matrix[up][left] or target > matrix[down][right]:
+                return False
+            
+            mid = left + (right-left) // 2
+            
+            row = up
+            while row <= down and matrix[row][mid] <= target:
+                if matrix[row][mid] == target:
+                    return True
+                row += 1
+            
+            return search(left, row, mid-1, down) or \
+                    search(mid + 1, up, right, row-1)
+        return search(0, 0, len(matrix[0]) - 1, len(matrix) - 1)
