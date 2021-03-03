21d9d2e1ea1a0e33856a3dd8c6711c2e5a46c853
Alan Song <alansong95@gmail.com>
2021-03-02 20:35:23 -0800

    912_sort_an_array.py: implement quick sort

--- 912_sort_an_array.py
+++ 912_sort_an_array.py
@@ -27,3 +27,29 @@ class Solution(object):
         res.extend(left[lp:])
         res.extend(right[rp:])
         return res
+
+# quick sort
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def qsort(lst, lo, hi):
+            if lo < hi:
+                p = partition(lst, lo, hi)
+                qsort(lst, lo, p-1)
+                qsort(lst, p+1, hi)
+
+        def partition(lst, lo, hi):
+            pivot = lst[hi]
+            i = lo
+            for j in range(lo, hi):
+                if lst[j] < pivot:
+                    lst[j], lst[i] = lst[i], lst[j]
+                    i += 1
+            lst[hi], lst[i] = lst[i], lst[hi]
+            return i
+            
+        qsort(nums, 0, len(nums)-1)
+        return nums
