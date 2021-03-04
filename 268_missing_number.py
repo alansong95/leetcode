4315acea14d93a67cde6037d96363cb7645f6023
Alan Song <alansong95@gmail.com>
2021-03-03 20:19:33 -0800

    268_missing_number.py: implement using hashset

--- 268_missing_number.py
+++ 268_missing_number.py
@@ -9,7 +9,8 @@ class Solution(object):
         return (_sum - sum(nums))
 
 
-# O(nlog(n))
+# time: O(nlog(n))
+# space: O(1)
 class Solution2(object):
     def missingNumber(self, nums):
         """
@@ -21,4 +22,18 @@ class Solution2(object):
             if i != nums[i]:
                 return i
         return len(nums)
-            
+
+
+# time: O(n)
+# space: O(n)
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hashset = set(nums)
+        
+        for i in range(len(nums)+1):
+            if i not in hashset:
+                return i
