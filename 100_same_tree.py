b67552ccf5cb54e32376852d09945366fa0f489d
Alan Song <alansong95@gmail.com>
2021-03-07 17:38:13 -0800

    100_same_tree.py: implement iteratively

--- 100_same_tree.py
+++ 100_same_tree.py
@@ -1,9 +1,4 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# recursive
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -18,3 +13,39 @@ class Solution(object):
         if p.val != q.val:
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+# iterative
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        def check(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+            
+        
+        deque = collections.deque([(p, q)])
+        
+        while deque:
+            p, q = deque.popleft()
+            if not check(p, q):
+                return False
+            if p:
+                deque.append((p.left, q.left))
+                deque.append((p.right, q.right))
+        return True
