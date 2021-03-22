977cdb37161d4926a2538bb6f2e1620fb821535f
Alan Song <alansong95@gmail.com>
2021-03-21 20:41:45 -0700

    1773_count_items_matching_a_rule.py: implement one liner

--- 1773_count_items_matching_a_rule.py
+++ 1773_count_items_matching_a_rule.py
@@ -13,3 +13,15 @@ class Solution(object):
             if item[keyIndex[ruleKey]] == ruleValue:
                 res += 1
         return res
+
+
+class Solution2(object):
+    keyIndex = {'type': 0, 'color': 1, 'name': 2}
+    def countMatches(self, items, ruleKey, ruleValue):
+        """
+        :type items: List[List[str]]
+        :type ruleKey: str
+        :type ruleValue: str
+        :rtype: int
+        """
+        return sum(1 for item in items if item[self.keyIndex[ruleKey]] == ruleValue)
