091df0b5a29ed59a033e9eba4cd6bffa2f56d733
Alan Song <alansong95@gmail.com>
2021-03-21 20:37:00 -0700

    complete: 1790_check_if_one_string_swap_can_make_strings_equal.py

---
+++ 1790_check_if_one_string_swap_can_make_strings_equal.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def areAlmostEqual(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        diff = [[x, y] for x, y in zip(s1, s2) if x != y]
+        return not diff or len(diff) == 2 and diff[0][::-1] == diff[1]
