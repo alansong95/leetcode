cbcfdef7185a197e2b722302ea443a84f1b933d4
Alan Song <alansong95@gmail.com>
2021-03-21 18:31:36 -0700

    766_toeplitz_matrix.py: implement without hashmap

--- 766_toeplitz_matrix.py
+++ 766_toeplitz_matrix.py
@@ -13,3 +13,16 @@ class Solution(object):
                 elif hashmap[col-row] != matrix[row][col]:
                     return False
         return True
+
+
+class Solution2(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        for row in range(len(matrix)-1):
+            for col in range(len(matrix[row])-1):
+                if matrix[row][col] != matrix[row+1][col+1]:
+                    return False
+        return True
