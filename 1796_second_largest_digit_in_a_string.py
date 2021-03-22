0577164d519f11f9fc02f0e17f6c6a3676feaf10
Alan Song <alansong95@gmail.com>
2021-03-21 20:33:40 -0700

    1796_second_largest_digit_in_a_string.py: implement linear solution

--- 1796_second_largest_digit_in_a_string.py
+++ 1796_second_largest_digit_in_a_string.py
@@ -10,4 +10,26 @@ class Solution(object):
         if len(nums) > 1:
             return nums[-2]
         return -1
+
+
+class Solution2(object):
+    def secondHighest(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        largest = -1
+        second_largest = -1
+        for c in s:
+            if c.isdigit():
+                c = int(c)
+                if c > largest:
+                    second_largest = largest
+                    largest = c
+                elif c > second_largest and c != largest:
+                    second_largest = c
+        return second_largest if largest != second_largest else -1
+                    
+        
+                    
         
