62ef7304bc76f1fe37573b0403e72330b24e9c2d
Alan Song <alansong95@gmail.com>
2021-03-22 18:06:11 -0700

    1736_latest_time_by_replacing_hidden_digits.py: implement more clean solution

--- 1736_latest_time_by_replacing_hidden_digits.py
+++ 1736_latest_time_by_replacing_hidden_digits.py
@@ -25,4 +25,25 @@ class Solution(object):
 
         
         
+class Solution2(object):
+    def maximumTime(self, time):
+        """
+        :type time: str
+        :rtype: str
+        """
+        ls = list(time)
+        for i, c in enumerate(ls):
+            if c == '?':
+                if i == 0:
+                    ls[0] = '2' if ls[1] < '4' or ls[1] == '?' else '1'
+                elif i == 1:
+                    ls[1] = '3' if ls[0] == '2' else '9'
+                elif i == 3:
+                    ls[3] = '5'
+                elif i == 4:
+                    ls[4] = '9'
+        return ''.join(ls)
+
+        
+        
         
